34da9de9f0c45c8566f9f33b2e68320161970761
Abdellah <AbdellahKarani>
2024-05-10 00:15:35 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,28 @@
-// Repeat The Set Data TYpes Please
+// Set vs WeakSet And Garbage Collector
+    
+/*
+    WeakSet is Weak , 
+    meaning refrence to object in weakset that help weakly.
+    if no other refrences to a object store, these object can be garbage collected.
+
+    Different Between Set And WeakSet
+    
+    ==> Set
+        Can Store Any Data Values
+        Have Size Proprety
+        Have [Keys, Values, Enteries]
+        Can Use foreach
+    ==> WeakSet
+        Collection Of Object Only
+        Does Have A Size Proprety
+        Does Not Have [clear, values, keys, enteries]
+        Can Not Use foreach
+*/
+
+
+
+
+
+
+
+
