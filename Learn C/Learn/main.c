0fd9f1386c72067d7cd2c71cb06e6e0ea1e5b413
Abdellah <AbdellahKarani>
2024-05-10 23:55:51 +0100

    .

--- Learn C/Learn/main.c
+++ Learn C/Learn/main.c
@@ -8543,41 +8543,6 @@ int main() {
 */
 
 /*
-int main() {
-    int n = 9, count = 0, a, b;
-    for (int i = 0; i < 2 * n; i++) {
-        a = n;
-        b = 1;
-        for (int j = 0; j < (2 * n) - 1; j++) {
-            if (i < n) {
-                if (j < n) {
-                    if (i <= j) printf("%d ",n - i);
-                    else printf("@ "); // "%d ",n - j
-                } else {
-                    if (j < (2 * n) - i - 1) printf("%d ",n - i);
-                    else {
-                        printf("/ "); // "%d ", n + b - i
-                        b++;
-                    }
-                }
-            } else {
-                if (j < n) {
-                    if (j < n - c)
-                } else {
-
-                }
-            }
-        }
-        printf("\n");
-        if (i < n - 1) count++;
-        else count--;
-        // Incremente The Variables
-    }
-    return 0;
-}
-*/
-
-/*
 // Exercice 3
 int * alloue_Vecteur(int t, int val) {
     int *vect = (int *) malloc(sizeof(int) * t);
@@ -8864,8 +8829,75 @@ int main() {
 }
 */
 
+/*
+///////// Start TD/TP 3
+
+typedef struct cell {
+    char *donne;
+    struct cell *next;
+    struct cell *prev;
+} Element;
+
+typedef struct {
+    char first;
+    char last;
+    int size;
+} Info;
+
+void intialiser(Info *in) {
+    in->first = 10; in->last = 20; in->size = 5;
+}
+
+int ajouter_ele(Element *ele) {
+
+}
+
+
 int main() {
-    printf("GOOD!");
+    // 1
+    Element *head = NULL;
+    // 2
+    Info in;
+    intialiser(&in);
+    return 0;
+}
+*/
+
+
+int main() {
+    int n = 9, count = 0, b, c = 0, a = 0, d = 0;
+    for (int i = 0; i < 2 * n - 1; i++) {
+        b = 1;
+        for (int j = 0; j < (2 * n) - 1; j++) {
+            if (i < n) {
+                if (j < n) {
+                    if (i <= j) printf("%d ",n - i);
+                    else printf("@ "); // "%d ",n - j
+                } else {
+                    if (j < (2 * n) - i - 1) printf("%d ",n - i);
+                    else {
+                        printf("/ "); // "%d ", n + b - i
+                        b++;
+                    }
+                }
+            } else {
+                if (j < n) {
+                    if (j < n - 1 - c) printf("* ");
+                    else printf("%d ", i - n + 2);
+                } else {
+                    if (j < n + d)printf("%d ", i - n + 2);
+                    else  printf("* ");
+                }
+
+            }
+        }
+
+        printf("\n");
+        if (i < n - 1) count++;
+        else {
+            count--; c++; d++;
+        }
+    }
     return 0;
 }
 
