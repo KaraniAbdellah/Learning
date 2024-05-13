ff8d4082235c57de8a274ac4a6ca4d3b559102c9
Abdellah <AbdellahKarani>
2024-05-13 22:26:52 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -3139,6 +3139,39 @@ console.log(newMap.get("Function"));
 
 
 
+// Map Data Types
+/*
+    Methods
+    -- Set 
+    -- get
+    -- delete 
+    -- clear
+    -- has
+    Proprioties
+        -- size
+    --> Map Is Sensible To The Key Letters
+*/
+
+var myMap = new Map([
+    [10, "Number"],
+    ["name", "String"],
+    [false, "Boolean"],
+]);
+myMap.set("10", "Number");
+console.log(myMap.get(10));
+console.log(myMap.get(false));
+
+console.log(myMap.size);
+console.log(myMap.delete("10")); // true --> Succeffuly Deleted
+console.log(myMap.size);
+console.log(myMap.clear());
+console.log(myMap.size);
+
+if (myMap.has("Name")) console.log(`There is 10 key`);
+else console.log("There Is No 10 Key");
+
+
+
 
 
 
