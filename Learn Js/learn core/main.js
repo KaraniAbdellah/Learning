ef3bca7157052fb767be3ca6d7c105d0fd1fe716
Abdellah <AbdellahKarani>
2024-05-13 01:09:37 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,29 @@
-/// do This Only Please --> Just TRy
+var simpleObject = {};
+var simpleMap = new Map;
+var simpleAn = Object.create(null);
+
+
+console.log(simpleAn);
+console.log(simpleMap);
+console.log(simpleObject);
+
+
+
+
+var new_object = {
+    10: "Number",
+    "10": "String",
+};
+
+var newMap = new Map();
+newMap.set("10", "String");
+newMap.set(10, "Number");
+
+console.log(new_object[10]);
+console.log(newMap.get(10));
+
+
+
+
+
+
