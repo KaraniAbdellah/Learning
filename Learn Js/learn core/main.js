994da8bfd980bfc2615fa52f32d9f4c6549fd4d5
Abdellah <AbdellahKarani>
2024-05-15 23:50:59 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -2,7 +2,6 @@
 
 /*
     WeakMap Allows Garbage Collector To Do Its Task But Not Map.
-
     Map     ==> Key Can Be Anything
     WeakMap ==> Key Can Be Object Only 
 */
@@ -10,20 +9,14 @@
 var muser = { theName: "Abdellah" };
 var myMap = new Map();
 myMap.set(muser, "Object Value");
-
 console.log(myMap);
-
-
 muser = null; // Overwrite The Refrence
 console.log(myMap);
-
 
 var wuser = { theName: "Abdellah" };
 var myWeakMap = new WeakMap();
 myWeakMap.set(wuser, "Object Value");
-
 console.log(myWeakMap);
-
 wuser = null; // Overwrite The Refrence
 console.log(myWeakMap); // null
 
