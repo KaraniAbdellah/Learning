c44e19f6876e102790a7cb9204b21dbf6dc37522
Abdellah <AbdellahKarani>
2024-05-18 00:38:49 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,28 +1,22 @@
-// Map Vs WeakMap
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
+// Arrays Methods
 /*
-    WeakMap Allows Garbage Collector To Do Its Task But Not Map.
-    Map     ==> Key Can Be Anything
-    WeakMap ==> Key Can Be Object Only 
+    - Array.from(Iterable, MapFunc, This)
+    --- Variable
+    --- String Numbers
+    --- Set
+    --- Using The Map Function
+    --- Arrow Function
+    --- Shorten The Method + USe Arguements 
 */
 
-var muser = { theName: "Abdellah" };
-var myMap = new Map();
-myMap.set(muser, "Object Value");
-console.log(myMap);
-muser = null; // Overwrite The Refrence
-console.log(myMap);
 
-var wuser = { theName: "Abdellah" };
-var myWeakMap = new WeakMap();
-myWeakMap.set(wuser, "Object Value");
-console.log(myWeakMap);
-wuser = null; // Overwrite The Refrence
-console.log(myWeakMap); // null
-
-
-
-
+console.log(Array.from("12345", function(number) {
+    return +number + +number;
+}));
+console.log(Array.from(1223));
 
 
 
