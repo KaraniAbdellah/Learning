da038b89edf94c60519c844425119fe5027b87ab
Abdellah <AbdellahKarani>
2024-05-20 01:23:28 +0100

    .

---
+++ Learn Js/learn core/main.js
@@ -0,0 +1,16 @@
+var newSet = new Set([1, 2, 3, 4, 4, 5, 1]);
+console.log(newSet);
+console.log(newSet[1]);
+
+console.log(newSet.delete(3));
+
+console.log(newSet);
+
+console.log(newSet.has(1));
+console.log(newSet);
+
+console.log(newSet.clear());
+console.log(newSet);
+
+
+
