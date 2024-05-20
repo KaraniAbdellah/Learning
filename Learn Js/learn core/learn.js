09da30e7ad6ddc7b748549597b4352f1eca73eac
Abdellah <AbdellahKarani>
2024-05-20 01:17:25 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -3196,6 +3196,31 @@ console.log(myWeakMap); // null
 
 
 
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// Arrays Methods
+/*
+    - Array.from(Iterable, MapFunc, This)
+    --- Variable
+    --- String Numbers
+    --- Set
+    --- Using The Map Function
+    --- Arrow Function
+    --- Shorten The Method + USe Arguements 
+*/
+console.log(Array.from("12345", function(number) {
+    return +number + +number;
+}));
+console.log(Array.from(1223));
+
+
+
+
+
+
+
+
 
 
 
