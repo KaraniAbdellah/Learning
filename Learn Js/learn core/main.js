9c06c7f3ac4b174328eb7fb7e5232b1179c1eb37
Abdellah <AbdellahKarani>
2024-05-21 21:44:20 +0100

    .

---
+++ Learn Js/learn core/main.js
@@ -0,0 +1,27 @@
+// Set is Data Type
+// Remove The Duplicates Elements
+
+
+var mySet = new Set([1, 2, 3, 4, 5, 7, 5]);
+console.log(mySet);
+
+mySet.add(12);
+mySet.delete(2);
+console.log(mySet);
+
+console.log(mySet[0]); // undefined
+
+if (mySet.has(2)) console.log("YES");
+else console.log("NO");
+
+console.log(mySet.size); // 6
+
+
+
+
+
+
+
+
+
+
