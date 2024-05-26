7358fcd3e86ed18d4b2fb8ead86ada3675307762
Abdellah <AbdellahKarani>
2024-05-26 23:50:07 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,8 +1,23 @@
-console.log(Array.from("12345", function(number) {
-    return +number + +number;
+
+console.log(Array.from("Abdellah"));
+console.log(
+    Array.from("12345", function(number) {
+        return +number + +number;
 }));
-console.log(Array.from(1223));
+console.log(Array.from("12345", (n) => +n + +n));
 
+// Use Cases 1
+var myArray = [1, 1, 1, 2, 3, 4];
+var myNewArray = new Set(myArray);
+console.log(myNewArray);
+console.log(Array.from(myNewArray));
+console.log([...new Set(myArray)]); // Future
 
+// Uses Cases 2
+function array_from() {
+    console.log(arguments);
+    return Array.from(arguments);
+}
+console.log(array_from("Abdellah", "karani", 30));
 
 
