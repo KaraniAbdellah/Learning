484b323c5ac39e3e042da8b8f2257c5f5e3929a0
Abdellah <AbdellahKarani>
2024-05-30 16:45:28 +0100

    .

---
+++ Fondamentls/With C/tree/tree.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
---
+++ Fondamentls/With C/tree/tree.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Start Learning Trees
+
+/*
+    - There Are Lot Of Types of Trees But We Are Going To Focus In Binary Search Tree (BTS)
+    - We Are Going To See Implementation of [Insertion, Delelation, Search, browse or Add Elements]
+*/
+
+
+int main() {
+    
+}
+
