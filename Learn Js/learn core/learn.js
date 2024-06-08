f139369b97182febfc6b96e0d27a6b32a1954783
Abdellah <AbdellahKarani>
2024-06-08 10:27:54 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -3321,7 +3321,7 @@ console.log(myArray);
   ------ Array => The Current Array Working With
   --- This Argument => Value To Use As This When Executing CallbackFunc
   Using
-  - Check if Element Exists In Array Suing Fucntion
+  - Check if Element Exists In Array Using Function
   - Check If Number In Range Using Object
 */
 var nums = [1, 2, 3, 4, 5, 6, 7];
@@ -3331,13 +3331,11 @@ var check = nums.some(function(ele) {
     return ele > this;
 }, myNumber);
 var check_nums = nums.some( ele => ele > 78);
-
 // With Using Function
 function checkNumber(arr, value) {
     return arr.some(ele => ele === value);
 }
 console.log(checkNumber(nums, 20)); // false
-
 // With Using Object
 var range = {
     min: 5,
@@ -3377,6 +3375,57 @@ var check = nbr_locations.every(function(ele) {
     return +ele > this;
 }, mainLocation);
 console.log(check); // false 10 is not greater 15
+
+
+
+
+/*
+  Spread Operator => ...Iterable
+  "Allow Iterable To Expand In Place"
+*/
+// Spread With String => Expand String
+
+console.log("Abdellah"); // Abdellah
+console.log(..."Abdellah"); // A b d e l l a h
+console.log([..."Abdellah"]); // ['A', 'b', 'd', 'e', 'l', 'l', 'a', 'h']
+
+// Concatenate Arrays
+
+var evenNums = [2, 4, 6, 8];
+var oddNums = [1, 3, 5, 7];
+console.log([...evenNums, ...oddNums]); // [2, 4, 6, 8, 1, 3, 5, 7]
+console.log(evenNums.concat(oddNums)); // [2, 4, 6, 8, 1, 3, 5, 7]
+
+// Copy Array
+
+var copiedArray = [...evenNums];
+console.log(copiedArray); // [2, 4, 6, 8]
+
+// Push Inside Array
+
+var allFriends = ["Abdelatif", "Muhamed", "Abdellah"];
+var thisYearFriends = ["Badr", "Ayman"];
+allFriends.push(...thisYearFriends);
+console.log(allFriends);
+
+// Use With Math Object
+
+var myNums = [10, 20, 30, 40, 50];
+console.log(Math.max(...myNums)); // 50
+
+// Spread With Objects => Merge Objects
+var objOne = {
+    a: 1,
+    b: 2,
+};
+var objTwo = {
+    c: 3,
+    d: 4,
+};
+console.log({...objOne, ...objTwo, e: 5}); // {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+
+
 
 
 
