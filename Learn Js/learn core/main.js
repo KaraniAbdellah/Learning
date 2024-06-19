8e8856478d415a49136ce72a88cdc27ab09c9719
Abdellah <AbdellahKarani>
2024-06-19 19:21:11 +0100

    D

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,17 @@
 // Repeat The Range
+
+
+var str = "Hello Abdellah How Are You ? Hello 4";
+var regex_str = /([^0-9][^0-9]llo [^0-9])|([^0-9][^0-9]llo [0-9])/ig;
+console.log(str.match(regex_str));
+
+
+
+
+
+
+
+
+
+
+
