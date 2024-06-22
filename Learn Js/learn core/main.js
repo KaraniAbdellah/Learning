894d04b7203c1c151858fd9b05d53f4faa451a05
Abdellah <AbdellahKarani>
2024-06-22 23:58:28 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,18 @@
-// Repeat All Classes Character
+// the chracter is 
+
+var str = "abdellah";
+// part 1
+var regx = /\w/g;
+var regx = /\W/g;
+var regx = /\d/g;
+var regx = /\D/g;
+var regx = /\s/g;
+var regx = /\S/g;
+
+var regx = /\babdellah/g;
+var regx = /\Babdellah\b/g;
+
+console.log(/\Babdellah\b/g.test(str));
+
+
+
