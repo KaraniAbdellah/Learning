a10c5ca808704a94269884b399e5c81857e7865f
Abdellah <AbdellahKarani>
2024-06-22 23:58:49 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,18 +1,1 @@
-// the chracter is 
-
-var str = "abdellah";
-// part 1
-var regx = /\w/g;
-var regx = /\W/g;
-var regx = /\d/g;
-var regx = /\D/g;
-var regx = /\s/g;
-var regx = /\S/g;
-
-var regx = /\babdellah/g;
-var regx = /\Babdellah\b/g;
-
-console.log(/\Babdellah\b/g.test(str));
-
-
-
+// Repeat Again
