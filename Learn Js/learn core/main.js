e713d4f927e67d822314cf2978ff988d8dfc106d
Abdellah <AbdellahKarani>
2024-06-29 12:26:07 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -3,24 +3,6 @@
 
 
 
-//  Regular Expressions - Quantifiers Part 2
-
-/*
-    n{x}   => Number of
-    n{x,y} => Range
-    n{x,}  => At Least x
-
-    x and y are numbers
-*/
-var serials = "S100S S3000S S50000S S950000S";
-
-console.log(serials.match(/s\d{3}s/gi)); // S[Three Number]S
-console.log(serials.match(/s\d{4,5}s/gi)); // S[Fout Or Five]S
-console.log(serials.match(/\s\d{4, }s/gi)); // S[At Least Four]S
-
-
-
-
 
 
 
