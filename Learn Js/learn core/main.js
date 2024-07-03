f56382ee72e5c79aaac615190de1b0fa9a58d00f
Abdellah <AbdellahKarani>
2024-07-03 16:39:11 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,18 +1,1 @@
-// Regular Expression – Replace With Pattern
-
-/*
-  Regular Expression
-  - replace --> replace first search value 
-  - replaceAll
-*/
-
-var txt = "We Love Programming And @ Because @ Is Amazing";
-console.log(txt.replace("@", "Js"));
-console.log(txt.replaceAll(/@/gi, "Js"));
-
-
-
-
-
-
-
+// repeat the regex last lessons
