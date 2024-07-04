478773cfc0fd8b64ce56790d3244e15f7c66dbb9
Abdellah <AbdellahKarani>
2024-07-04 21:39:07 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,17 @@
-// repeat the regex last lessons
+// Practice Form Validation
+
+var form = document.getElementById("register");
+form.addEventListener("submit", function(event) {
+    var phone_input = document.getElementById("phone").value;
+    var phone_regex = /\(\d{4}\)\s\d{3}-\d{4}/; // (1234) 567-8910
+    if (phone_regex.test(phone_input)) {
+        return true;
+    }
+    event.preventDefault();
+    return false;
+});
+
+
+
+
+
