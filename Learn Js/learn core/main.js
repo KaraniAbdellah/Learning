995a76ef70d0d791fc45d4edc0c1ccc7ac11813e
Abdellah <AbdellahKarani>
2024-07-04 21:39:43 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,17 +1,1 @@
-// Practice Form Validation
-
-var form = document.getElementById("register");
-form.addEventListener("submit", function(event) {
-    var phone_input = document.getElementById("phone").value;
-    var phone_regex = /\(\d{4}\)\s\d{3}-\d{4}/; // (1234) 567-8910
-    if (phone_regex.test(phone_input)) {
-        return true;
-    }
-    event.preventDefault();
-    return false;
-});
-
-
-
-
-
+// Do A Project With From Validation --> Please Write It Note
