8e7476d4fc0e5eebff4c6062fc397e7a46dd2a16
Abdellah <AbdellahKarani>
2024-07-12 23:45:11 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,27 +1,25 @@
+// Update Properties And Built In Constructors
+
+
 class User {
     constructor(id, username, salary) {
         this.i = id;
         this.u = username || "Unknown";
         this.s = salary < 6000 ? salary + 500 : salary;
-        this.msg = function() {
-            return ("Hello ", this.u);
-        }
     }
-    // Methods
-    writeMessage() {
-        return ("Hello ", this.u);
-    }
+    UpdateName(new_name) {
+        this.u = new_name;
+    }    
 }
 
-var user1 = new User(10, "", 2000);
-var user2 = new User(100, "mohamed", 7000);
+var user1 = new User(10, "abdellah", 2000);
 
-console.log(user1.i, user1.u, user1.s);
-console.log(user2.i, user2.u, user2.s);
-console.log(user2.msg()); // Hello Mahamed
-console.log(user2.msg); // native code
-console.log(user2.writeMessage());
-console.log(user2.writeMessage); // native code
+console.log(user1.u);
+user1.UpdateName("mohamed");
+console.log(user1.u);
+
+
+
 
 
 
