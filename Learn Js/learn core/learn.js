7b279cd860becd1ad30a9fac6fad0a157c46c019
Abdellah <AbdellahKarani>
2024-07-15 15:57:38 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -3820,3 +3820,40 @@ console.log(Ms_Director.e);
 console.log(Ms_Director.sayHello()); // Hello Abdellah, Your Email is null
 
 
+
+// Encapsulation
+/*
+    - Class fields are open to everyone unless specified otherwise.
+    - Ensures data is accessed properly.
+    - Allows you to use the app without needing to understand the complex parts.
+    - Helps avoid human errors.
+    - Makes the app more adaptable and easier to manage.
+    - Simplifies the overall app.
+*/
+class User {
+    // Private Proprety
+    #is
+    #sayHello
+    constructor(name, id, isManager) {
+        this.i = id;
+        this.n = name;
+        this.#is = isManager; // private proprety
+        this.#sayHello = this.#createHelloMessage();
+    }
+    // Private Method
+    #createHelloMessage() {
+        return `Hello, ${this.n}! Manager status: ${this.#is}`;
+    }
+    // Public Method
+    greet() {
+        return this.#sayHello;
+    }
+}
+var userOne = new User("Abdellah", 2321, true);
+console.log(typeof User); // function
+// consolee.log(userOne.#is); // is not accessible outside class 'User'
+console.log(userOne.greet());
+
+
+
+
--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,34 @@
-// Please Repeat With Given Example
+// Encapsulation
+/*
+    - Class fields are open to everyone unless specified otherwise.
+    - Ensures data is accessed properly.
+    - Allows you to use the app without needing to understand the complex parts.
+    - Helps avoid human errors.
+    - Makes the app more adaptable and easier to manage.
+    - Simplifies the overall app.
+*/
+class User {
+    // Private Proprety
+    #is
+    #sayHello
+    constructor(name, id, isManager) {
+        this.i = id;
+        this.n = name;
+        this.#is = isManager; // private proprety
+        this.#sayHello = this.#createHelloMessage();
+    }
+    // Private Method
+    #createHelloMessage() {
+        return `Hello, ${this.n}! Manager status: ${this.#is}`;
+    }
+    // Public Method
+    greet() {
+        return this.#sayHello;
+    }
+}
+var userOne = new User("Abdellah", 2321, true);
+console.log(typeof User); // function
+// consolee.log(userOne.#is); // is not accessible outside class 'User'
+console.log(userOne.greet());
+
+
