82e3a1a63f1f9176d15ca4b0619bfcc8eddaf155
Abdellah <AbdellahKarani>
2024-07-16 13:26:19 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,4 +1,17 @@
-// do the example for inhertance from class that has private proprety
+// Undersatnd this please
 
+class Parent {
+    #privateProperty = 'I am private';
+    getPrivateProperty() {
+        return this.#privateProperty;
+    }
+}
 
+class Child extends Parent {
+    showPrivateProperty() {
+        return this.getPrivateProperty(); // Accessing via a public method
+    }
+}
 
+const child = new Child();
+console.log(child.showPrivateProperty()); // Output: 'I am private'
