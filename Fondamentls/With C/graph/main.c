59557259fc9a9b3c1e6a000c5bfaf5c3a061a434
Abdellah <AbdellahKarani>
2024-07-19 20:45:58 +0100

    .

--- Fondamentls/With C/graph/main.c
+++ Fondamentls/With C/graph/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include "myLib.h" // this contain the prototypes functions
+#include <stdlib.h>
+#include "myLib.h"
 
 
 
 
 int main() {
+    // graphe simple oriented
     
-    
-    
+    for (int i = 0; i < n; i++) {
+        adjList[i] = (List *) malloc(sizeof(List));
+        adjList[i]->head = NULL;
+    }
+    addNode(0, 1);
+    addNode(0, 2);
+    addNode(1, 2);
+    printfList();
     return 0;
 }
 
--- Fondamentls/With C/graph/myLib.h
+++ Fondamentls/With C/graph/myLib.h
@@ -1,13 +1,61 @@
-// Start The Prototype Of The Functions
+// simple graph oriented
+#include <stdlib.h>
+
+#define n 3
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct List {
+    Node *head;
+} List; 
+
+List *adjList[n] = {0};
+
+void addNode(int s, int d) {
+    Node *dest, *src, *temp;
+    if (adjList[s]->head == NULL) {
+        src = (Node *) malloc(sizeof(Node));
+        src->data = s;
+        src->next = NULL;
+        adjList[s]->head = src;
+    }
+    dest = (Node *) malloc(sizeof(Node));
+    dest->data = d;
+    temp = adjList[s]->head;
+    while (temp->next != NULL) temp = temp->next;
+    dest->next = temp;
+}
 
 
-#ifndef MYLIB_H
-	#define MYLIB_H
-	// Start Prototype Fucntions
-	
-	
-	
-	
-#endif
+void printfList() {
+    for (int i = 0; i < n; i++) {
+        Node *temp = adjList[i]->head;
+        printf("Adjacency List for %d are : ", i);
+        while (temp != NULL) {
+            printf("%d  ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
