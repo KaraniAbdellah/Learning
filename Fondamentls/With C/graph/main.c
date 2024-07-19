5f5ba7fc44c6f695a4c880fbd9c8feea7719d159
Abdellah <AbdellahKarani>
2024-07-19 20:15:57 +0100

    .

--- Fondamentls/With C/graph/main.c
+++ Fondamentls/With C/graph/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "myLib.h" // this contain the prototypes functions
+
+
+
 
 int main() {
+    
+    
     
     return 0;
 }
---
+++ Fondamentls/With C/graph/myLib.h
@@ -0,0 +1,13 @@
+// Start The Prototype Of The Functions
+
+
+#ifndef MYLIB_H
+	#define MYLIB_H
+	// Start Prototype Fucntions
+	
+	
+	
+	
+#endif
+
+
