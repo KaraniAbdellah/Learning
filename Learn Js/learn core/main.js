a1812a39747d4a0408aa2f20826f95437f816f3d
Abdellah <AbdellahKarani>
2024-07-27 09:27:27 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,52 +1,43 @@
-// revise the static propreties
-
-
-
+// revise the private method
 class User {
-    count = 0; // for Object
+    count = 0;
     static nbr = 0;
-    constructor(id, name, email) {
+    // private proprety
+    #talk
+    #thing
+    constructor(email, id, name) {
         this.e = email;
-        this.n = name;
         this.i = id;
+        this.n = name;
+        this.count += 1;
         User.nbr += 1;
-        this.msg  = function() {
-            return `No Message ${this.e}`; 
-        }
+        this.#talk = "Hello World"; // private
+        this.#thing = this.#saySomeThing();
     }
-    static state() {
-        return `The Number Of is :  ${this.nbr}`;
+    message () {
+        return this.#talk;
+    }
+    #saySomeThing() {
+        return this.message();
+    }
+    messageOfThings() {
+        return this.#saySomeThing();
     }
 }
 
-var userOne = new User(1000, "john", "john@gmain.com");
-
-console.log(userOne.msg(), userOne.e); 
+var userOne = new User("a@gmail.com", 1000, "abdellah");
 console.log(userOne.count); // 0
-console.log(User.count); // undefined
+console.log(User.count); // unidefined
 console.log(User.nbr); // 1
-console.log(userOne.nbr); // undefined
-console.log(User.state());
+console.log(userOne.nbr); // undifined
+console.log("-".repeat(30));
 
+console.log(User.talk); // undifined
+console.log(userOne.talk); // undifined
+console.log("-".repeat(30));
 
-// Inhertence
-
-/**
- * the admin inhert the property with there value
- * we can inhert a static property or method
- */
-
-class Admin extends User {
-    constructor (email, id, name, salary) {
-        super(id, name, email);
-        this.s = salary;
-    }
-}
-
-var adminOne = new Admin("admin@gmail.com", 10203, "deo", 2000);
-
-console.log(adminOne.count); // 0
-console.log(Admin.state()); // The Number Is 2 [ user and admin ]
+console.log(userOne.message());
+console.log(userOne.messageOfThings());
 
 
 
