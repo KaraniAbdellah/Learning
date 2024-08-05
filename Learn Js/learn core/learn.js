9a738d679adcf50c62e42ade9eae0ffbd057b51a
Abdellah <AbdellahKarani>
2024-08-05 11:41:31 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -4161,7 +4161,7 @@ console.log(dateNow);
 console.log(dateNow); // Thu Jan 01 1970 00:00:01
 
 // dateNow.setDate(10);
-console.log(dateNow); // Sat Aug 10 2024 18:18:15 
+console.log(dateNow); // Sat Aug 10 2024 18:18:15
 
 // dateNow.setFullYear(2020, 11, 7);
 console.log(dateNow); // Mon Dec 07 2020 18:21:17
@@ -4179,6 +4179,39 @@ dateNow.setSeconds(8, 100);
 console.log(dateNow); // Sun Aug 04 2024 18:29:08
 
 
+
+
+
+// Formatting Date And Time
+/*
+    new Date(timestamp)
+    new Date(Date String)
+    new Date(Numeric Values)
+
+    Format
+    - "Jan 18 2005"
+    - "01/18/2005"
+    - "2005-01-18" => ISO International Standard
+    - "2005 01"
+    - "2005"
+    - "82" --> 1982
+    - 2005, 9, 18, 2, 1, 0
+    - 2005, 9, 18
+    - "2005-01-25T06:01:00Z" --> Z [ Time Zone ]
+
+    Date.parse("String") // Read Date From A String And Return Date In MillSecond
+*/
+var myBrithday = Date.parse("Jan 18 2005");
+console.log(myBrithday); // 1106006400000
+
+var date1 = new Date(myBrithday);
+console.log(date1);
+
+var date2 = new Date(2005, 1, 18, 14, 90, 90);
+console.log(date2); // Fri Feb 18 2005 15:31:30
+
+var date3 = new Date("2005-01-25T06:01:00Z");
+console.log(date3);
 
 
 
--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,33 @@
-// repeat all thing in date and time please
+// Formatting Date And Time
+/*
+    new Date(timestamp)
+    new Date(Date String)
+    new Date(Numeric Values)
+
+    Format
+    - "Jan 18 2005"
+    - "01/18/2005"
+    - "2005-01-18" => ISO International Standard
+    - "2005 01"
+    - "2005"
+    - "82" --> 1982
+    - 2005, 9, 18, 2, 1, 0
+    - 2005, 9, 18
+    - "2005-01-25T06:01:00Z" --> Z [ Time Zone ]
+
+    Date.parse("String") // Read Date From A String And Return Date In MillSecond
+*/
+var myBrithday = Date.parse("Jan 18 2005");
+console.log(myBrithday); // 1106006400000
+
+var date1 = new Date(myBrithday);
+console.log(date1);
+
+var date2 = new Date(2005, 1, 18, 14, 90, 90);
+console.log(date2); // Fri Feb 18 2005 15:31:30
+
+var date3 = new Date("2005-01-25T06:01:00Z");
+console.log(date3);
+
+
+
