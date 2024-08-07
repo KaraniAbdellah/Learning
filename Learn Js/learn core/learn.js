e4d733c5587bffa24c4afa9e6f7b7bfcd1c12dbe
Abdellah <AbdellahKarani>
2024-08-07 19:39:32 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -4253,7 +4253,7 @@ console.log(duration);
         - calculates the time between 'startLoop' and 'endLoop',
             and stores it with the name 'loopDuration'.
 
-    performance.getEntriesByName('loopDuration')[0]
+    performance.getEntriesByName('loopDuration')[0] // RETURN ARRAY CONTAIN OBJECT
         - shows the time taken in milliseconds
 */
 // Perfomance.now()
@@ -4273,6 +4273,60 @@ performance.measure("LoopTime", "startLoop", "endLoop");
 // Get the measurement result
 var measure = performance.getEntriesByName("LoopTime")[0];
 console.log(`Loop Take ${measure.duration} Millseconds`); // Loop Take 0.2 Millseconds
+
+
+
+
+
+
+// Generator Function Introduction
+/*
+  Generators
+    - Generator Function Run Its Code When Required.
+    - Generator Function Return Special Object [Generator Object]
+    - Generators Are Iterable
+    // Example
+        show more  in some website when you click i generate another data
+    // generator
+        generator.next() --> return object with two property
+            value : yield value
+            done : false --> there is other yields, true --> no yield still
+        alert("Hello") --> Come When We Start Second yield
+*/
+
+function *generateNumber() {
+    yield 1;
+    console.log("Hello After Yield 1");
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}
+// Generator
+var generator = generateNumber();
+console.log(typeof generator); // object
+console.log(generator);
+
+var first_yield = generator.next();
+console.log(first_yield); // {value: 1, done: false}
+console.log(first_yield.value); // 1
+console.log(first_yield.done); // false
+
+console.log(generator.next()); // Hello && // {value: 2, done: false}
+console.log(generator.next()); 
+console.log(generator.next()); 
+console.log(generator.next()); 
+console.log(generator.next()); // {value: undefined, done: true}
+
+// Generator With Loop
+for (let value of generateNumber()) {
+    console.log(value); // 1 "Hello After Yield 1" 2 3 4 5
+}
+
+// Tell Me What is The Result
+for (let value of generator) {
+    console.log(value);
+}
 
 
 
--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,54 @@
-// repeat THis Please
+// Generator Function Introduction
+/*
+  Generators
+    - Generator Function Run Its Code When Required.
+    - Generator Function Return Special Object [Generator Object]
+    - Generators Are Iterable
+    // Example
+        show more  in some website when you click i generate another data
+    // generator
+        generator.next() --> return object with two property
+            value : yield value
+            done : false --> there is other yields, true --> no yield still
+        alert("Hello") --> Come When We Start Second yield
+*/
+
+function *generateNumber() {
+    yield 1;
+    console.log("Hello After Yield 1");
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}
+// Generator
+var generator = generateNumber();
+console.log(typeof generator); // object
+console.log(generator);
+
+var first_yield = generator.next();
+console.log(first_yield); // {value: 1, done: false}
+console.log(first_yield.value); // 1
+console.log(first_yield.done); // false
+
+console.log(generator.next()); // Hello && // {value: 2, done: false}
+console.log(generator.next()); 
+console.log(generator.next()); 
+console.log(generator.next()); 
+console.log(generator.next()); // {value: undefined, done: true}
+
+// Generator With Loop
+for (let value of generateNumber()) {
+    console.log(value); // 1 "Hello After Yield 1" 2 3 4 5
+}
+
+// Tell Me What is The Result
+for (let value of generator) {
+    console.log(value);
+}
+
+
+
+
+
+
