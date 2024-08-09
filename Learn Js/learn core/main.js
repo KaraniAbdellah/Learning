27f1f6a3c4629c4e6f157682c521273b49d9c9a4
Abdellah <AbdellahKarani>
2024-08-09 17:56:24 +0100

    .

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,3 +1,50 @@
-// repeat last two lessosns [ Delegate Generator, Infinit Numbers ]
+// Delegate Generator And Infintif Numbers
+
+
+function *genarateNumbers() {
+    yield 1;
+    yield 2;
+}
+
+function *generateLetters() {
+    yield "A";
+    yield "B";
+}
+
+
+function *generateAll() {
+    yield *generateLetters();
+    yield *genarateNumbers();
+    yield *["L", "L", "C"];
+    
+    let index = 0;
+    while (1) {
+        yield index++;
+        if (index > 3) break;
+    }
+}
+
+let generator = generateAll();
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next()); // {value: undefined, done: true}
+
+
+
+
+
+
+
+
 
 
