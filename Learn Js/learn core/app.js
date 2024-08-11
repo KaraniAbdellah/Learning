163402835c60034224a0f2c8ce13fd2e91186ee3
Abdellah <AbdellahKarani>
2024-08-11 12:03:50 +0100

    .

--- Learn Js/learn core/app.js
+++ Learn Js/learn core/app.js
@@ -1,1 +1,23 @@
-// repeat lesson
+
+// import {T, number} from "./main.js";
+// import * as all from "./main.js"; 
+
+// console.log(all.T); // [1, 2, 3, 4]
+// console.log(all.a); // 10
+// console.log(all.sayHello()); // Hello
+
+// import sayHello, {T, a} from "./main.js"; 
+
+// console.log(T); // [1, 2, 3, 4]
+// console.log(a); // 10
+// console.log(sayHello());  // Hello
+
+
+
+import {T, number, sayHello} from "./main.js";
+console.log(T);
+console.log(number);
+console.log(sayHello());
+
+
+
--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,1 +1,28 @@
-// repeat The Lesson please
+/**
+ * Import All
+ * Export Named
+ * Export Default
+ */
+
+
+// export let a = 10;
+// export let T = [1, 2, 3, 4];
+
+// export default function sayHello() {
+//     return `Hello`;
+// }
+
+let a = 10;
+let T = [1, 2, 3, 4];
+function sayHello() {
+    return `Hello`;
+}
+
+export {a as number, T, sayHello}
+
+
+
+
+
+
+
