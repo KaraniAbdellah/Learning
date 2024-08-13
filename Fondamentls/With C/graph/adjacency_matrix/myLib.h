42db84264bc51f832a91aade6934ef68195653f4
Abdellah <AbdellahKarani>
2024-08-13 20:30:16 +0100

    .

--- Fondamentls/With C/graph/adjacency_matrix/myLib.h
+++ Fondamentls/With C/graph/adjacency_matrix/myLib.h
@@ -1,1 +1,22 @@
 // start all implementaion here
+
+
+
+
+typedef struct graph {
+    int nbr_nodes;
+    int **edges;
+} graph;
+
+
+// create graph
+// add edges
+// distroy graph
+// print a graph
+
+
+
+
+
+
+
