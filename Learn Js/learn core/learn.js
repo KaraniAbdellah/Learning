c573dc58f3cfc1367b66afc2c7a7d208d69bdf0b
Abdellah <AbdellahKarani>
2024-08-15 14:39:23 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -4615,3 +4615,45 @@ console.log(typeof myJsonObject_ToServer); // string
 console.log(myJsonObject_ToServer); // {"Username":"Ahmed","Age":10}
 
 
+
+
+
+// Asynchronous vs Synchronous Programming
+/*
+    To Understand Ajax, Fetch, Promises
+
+    Asynchronous vs Synchronous Programming
+    - Meaning
+
+    Synchronous
+    - Operations Runs in Sequence
+    - Each Operation Must Wait For The Previous Operations To Complete
+    - Story From Real Life
+
+    Asynchronous
+    - Operations Runs In Parallel
+    - This Means That An Operation Can Run while Another One Is Still Being Processed
+    - Story From Real Life
+
+    - Facebook As Example
+    - Simulation
+
+    Search
+    - JavaScript Is A Single-Threaded
+    - Multi Threaded Languages
+*/
+
+// Synchronous
+// console.log("1");
+// console.log("2");
+// window.alert("Operation");
+// console.log("3");
+// Output : 1 2 Operation 3
+
+// Asynchronous
+console.log("1");
+console.log("2");
+setTimeout(() => console.log("Operation"), 3000);
+console.log("3");
+// Output : 1 2 3 Operations
+
