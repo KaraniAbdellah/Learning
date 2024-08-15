ef72476184009e87897272842803b4c8d96faecb
Abdellah <AbdellahKarani>
2024-08-15 14:30:54 +0100

    j

--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,6 +1,24 @@
-// Revise The Previos Lessons
+// Parse and Stringify
+/*
+    JSON
+    - JSON.parse => Convert JSON To JS Object
+    - JSON.stringify => Convert JS Object To JSON
+*/
+// Get The Data From Server
+const myJsonObject_FromSever = '{"Username": "Abdellah", "Age": 40}';
+console.log(typeof myJsonObject_FromSever); // string
+console.log(myJsonObject_FromSever); // {"Username": "Abdellah", "Age": 40}
 
+// Convert Json Object To JS Object
+const myJsObject = JSON.parse(myJsonObject_FromSever);
+console.log(typeof myJsObject); // object
+console.log(myJsObject); // {Username: 'Abdellah', Age: 40}
+myJsObject["Username"] = "Ahmed";
+myJsObject["Age"] = 10;
 
-
+// Convert Js Object To JSON Object
+const myJsonObject_ToServer = JSON.stringify(myJsObject);
+console.log(typeof myJsonObject_ToServer); // string
+console.log(myJsonObject_ToServer); // {"Username":"Ahmed","Age":10}
 
 
