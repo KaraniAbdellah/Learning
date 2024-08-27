5d72125573c2450838eab8d0a28afabf057d7e5f
Abdellah <AbdellahKarani>
2024-08-27 22:41:57 +0100

    .

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -5228,3 +5228,50 @@ fetch("https://api.github.com/users/KaraniAbdellah").then((result) => {
 }).catch((reject) => {
   console.log(Error("API_LINK"))
 });
+
+
+
+
+
+
+// Promise All And All Settled And Race
+/*
+    - All
+        get all resolve promises
+        if one rejected --> get the rejected and stop others promises
+    - All Settled
+        get all resolve and rejecte promises
+    - Race
+        get first resolve or rejecte promise
+*/
+
+var Pro1 = new Promise((res, rej) => {
+    res("I Am The First Promise");
+});
+var Pro2 = new Promise((res, rej) => {
+    res("I Am The Second Promise");
+});
+var Pro3 = new Promise((res, rej) => {
+    res("Rejected From Third Promise");
+});
+
+Promise.all([Pro1, Pro2, Pro3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+Promise.allSettled([Pro1, Pro2, Pro3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+Promise.race([Pro1, Pro2, Pro3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+
+
+
+
+
--- Learn Js/learn core/main.js
+++ Learn Js/learn core/main.js
@@ -1,4 +1,41 @@
-// revise fetch api
+// Promise All And All Settled And Race
+/*
+    - All
+        get all resolve promises
+        if one rejected --> get the rejected and stop others promises
+    - All Settled
+        get all resolve and rejecte promises
+    - Race
+        get first resolve or rejecte promise
+*/
+
+
+const myPromise1 = new Promise((res, rej) => {
+    res("I Am The First Promise");
+});
+const myPromise2 = new Promise((res, rej) => {
+    res("I Am The Second Promise");
+});
+const myPromise3 = new Promise((res, rej) => {
+    res("Rejected From Third Promise");
+});
+
+Promise.all([myPromise1, myPromise2, myPromise3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+Promise.allSettled([myPromise1, myPromise2, myPromise3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+Promise.race([myPromise1, myPromise2, myPromise3])
+.then((msgs) => {
+    console.log(msgs);
+});
+
+
 
 
 
