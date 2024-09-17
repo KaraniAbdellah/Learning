484dccfda94068fd1060a732413edd936e783972
abdellah karani <abdellahkarani@gamil.com>
2024-09-17 20:44:57 +0100

    .

--- Learn Java/Learning/learn.java
+++ Learn Java/Learning/learn.java
@@ -550,9 +550,61 @@ public class learn {
 
 /////////////////////////////////// 
 /////////////////////////////////// 
-// Methods/ Functions
+// Methods-Functions
 /////////////////////////////////// 
 /////////////////////////////////// 
 
 
-// Method / Function Part 1
+// Methods-Functions Part 1
+/*
+    Syntax:
+        type name(args) {
+            // code 
+            return value;
+        }
+    type: [int, float, void, ....]
+
+    Function Call:
+        static void print() --> without using object to call
+        void print() --> you need a object like intermediate
+        public static void print() --> i can this function from any file
+
+    Function With Argument:
+        static int sum(int x, int y) --> return integer
+    
+    EveryThing Inder Class
+*/
+
+/*
+public class learn {
+    // print message
+    public static void print() {
+        System.out.println("Hello World");
+        return;
+    }
+    // sum two numbers
+    static int sum(int x, int y) {
+        print();
+        return x + y;
+    }
+    public static void main(String args[]) {
+        print();
+        print();
+        int re = sum(10, 10);
+        System.out.printf("sum = %d%n", re);
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
