5eb12fbd8ce7dd05a57a16c77c8429e640789d46
abdellah karani <abdellahkarani@gamil.com>
2024-09-17 19:42:28 +0100

    .

--- Learn Java/Learning/learn.java
+++ Learn Java/Learning/learn.java
@@ -490,3 +490,69 @@ public class App {
     }
 }
 */
+
+
+
+
+// labeled break & labeled continue
+/*
+    label: is name of block of code
+    use break with label to stop block code execution
+    break stops the block of code
+
+    stop:{
+        int i = 9;
+        if (i == 0) break stop;
+        System.out.println("i != 0");
+    }
+*/
+
+/*
+public class learn {
+    public static void main(String args[]) {
+        // labeled break example
+        innerLoop:{
+            for (int i = 0; i < 10; i++) {
+                outerLoop:for (int j = 0; j < 5; j++) {
+                    if (i < j) {
+                        break outerLoop;
+                    }
+                    if (i == 5) {
+                        break innerLoop;
+                    }
+                    System.out.print("* ");
+                }
+                System.out.println("");
+            }
+        }
+        stop:{
+            int i = 9;
+            if (i == 0) break stop;
+            System.out.println("i is defrenct of zero");
+        }
+
+        // labeled continue;
+        nextRow: for (int i = 0; i < 5; i++) {
+            System.out.println("");
+            for (int j = 0; j < 10; j++) {
+                if (i < j) continue nextRow;
+                System.out.print("* ");
+            }
+            System.out.printf("#line %d", i + 1);
+        }
+    }
+}
+*/
+
+
+
+
+
+/////////////////////////////////// 
+/////////////////////////////////// 
+// Methods/ Functions
+/////////////////////////////////// 
+/////////////////////////////////// 
+
+
+// Method / Function Part 1
