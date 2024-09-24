7b0218b539eb887817388a96d1a039ef6db7abbd
abdellah karani <abdellahkarani@gamil.com>
2024-09-24 23:50:43 +0100

    .

--- Learn Java/Learning/learn.java
+++ Learn Java/Learning/learn.java
@@ -568,6 +568,7 @@ public class learn {
         static void print() --> without using object to call
         void print() --> you need a object like intermediate
         public static void print() --> i can this function from any file
+            --- access type [public, private, protected, ...]
 
     Function With Argument:
         static int sum(int x, int y) --> return integer
@@ -596,6 +597,44 @@ public class learn {
 }
 */
 
+
+
+
+
+// Variable Number Of Arguments & forEach
+/*
+    void sum(int...nums) // nums is an array
+    for (int i:nums) {
+        s += i;
+    }
+*/
+/*
+public class App {
+    protected static void find_largest(float a, float b, float c) {
+        float max = a;
+        if (max < b) max = b;
+        if (max < c) max = c;
+        System.out.println("the max number is " + max); 
+    }
+    private static float averge(float n1, float n2, float n3) {
+        return (n1 + n2 + n3) / 3;
+    }
+    public static int sum(int...nums) {
+        int s = 0;
+        for (int i:nums) {
+            s += i;
+        }
+        return s;
+    }
+
+    public static void main(String argc[]) {
+        float n1 = 10.2f, n2 = 10.4f, n3 = 90.3f;
+        find_largest(n1, n2, n3);
+        System.err.printf("The Averge Number is %f%n", averge(n1, n2, n3));
+        System.out.println("Sum = " + sum(10, 19, 32, 34, 29, 23));
+    }
+}
+*/
 
 
 
