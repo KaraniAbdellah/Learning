91b7175e50f1f40253b1e5446461970afebd9105
abdellah karani <abdellahkarani@gamil.com>
2024-09-25 20:44:14 +0100

    .

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,3 +1,24 @@
 //  Built in Method / Function
 
+public class App {
+    public static int sum(int...nums) {
+        int s = 0;
+        for (int i: nums) {
+            s += i;
+        }
+        return s;
+    }
+    public static void main(String main[]) {
+        System.out.println("The Sum Is " + sum(10, 10, 92, 28));
+    }
+}
+
+
+// search for methods please
+    // what is public and static in fucntions
+
+
+
+
+
 
