1b619d4947df3b0014427aa1719a9455b600a153
abdellah karani <abdellahkarani@gamil.com>
2024-10-02 00:00:42 +0100

    .

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,48 +1,5 @@
+// find the solution & revise the last lessons
 
-import java.util.Scanner;
-
-// One Dimensional Array (Char Array)
-/*
-    with array of character --> you can print without loop
-    you can casting --> (char) 97 --> a
-    there is no NULL terminator
-    in.next().toCharArray() --> if there is space --> stop printing
-    in.nextLine().toCharArray() --> continue printing
-
-    toCharArray() --> convert string to character array
-
-    why we use array of character
-        - quick
-        - space
-        - storage for example the password
-*/
-
-/* 
-public class App {
-    public static void main(String[] args) {
-        char arr[] = {72, 73};
-        System.err.println("msg = " + arr); // HI
-
-        char name[] = new char[10];
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        name = str.toCharArray();
-        // name = in.next().toCharArray();
-        System.out.println("name = " + name);
-
-        String extra = new String(name, 0, 3);
-        System.out.println("extra = " + extra);
-    }
-}
-*/
-
-
-
-
-
-
-
-
-
+// revise the array of character at the morining
 
 
