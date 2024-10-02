3327417c338d5fd52703014758e47122b6c255e8
abdellah karani <abdellahkarani@gamil.com>
2024-10-02 18:29:14 +0100

    .

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,5 +1,25 @@
-// find the solution & revise the last lessons
+// import java.util.Arrays;
+// import java.util.Scanner;
+/*
+public class App {
+    public static void main(String[] args) {
+        // define an array name
+        char name[] = new char[5];
+        name[0] = 'd';
+        System.out.printf("name = %s%n", Arrays.toString(name));
 
-// revise the array of character at the morining
+        // define a string and using array of character
+        Scanner in = new Scanner(System.in);
+        System.err.println("Enter A String : ");
+        String last_name = in.next();
+        System.err.println("The last name = " + last_name);
 
+        // convert string to array of character
+        char copy_arr[] = last_name.toCharArray();
+        System.out.println("The Char Array = " + copy_arr); 
 
+    }    
+}   
+*/
+
+    
--- Learn Java/Learning/learn.java
+++ Learn Java/Learning/learn.java
@@ -834,6 +834,39 @@ public class App {
 
 
 
+// Array Character Examples
+// import java.util.Arrays;
+// import java.util.Scanner;
+
+/*
+public class App {
+    public static void main(String[] args) {
+        // define an array name
+        char name[] = new char[5];
+        name[0] = 'd';
+        System.out.printf("name = %s%n", Arrays.toString(name));
+
+        // define a string and using array of character
+        Scanner in = new Scanner(System.in);
+        System.err.println("Enter A String : ");
+        String last_name = in.next();
+        System.err.println("The last name = " + last_name);
+
+        // convert string to array of character
+        char copy_arr[] = last_name.toCharArray();
+        System.out.println("The Char Array = " + copy_arr); 
+
+    }    
+}   
+*/
+
+    
+
+
+
+
+
+
 
 
 
