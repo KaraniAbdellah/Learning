065a213d3ba65d8c5dcc07d3c63293c7a2f9d3b1
abdellah karani <abdellahkarani@gamil.com>
2024-10-02 19:25:39 +0100

    /

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -5,6 +5,8 @@
 // try to work with java in problem solving and why not "python" also
 
 
-
+// understand the concepts of:
+    // call by refrence with array in fucntions
+    // call by value with varaibles in functions
 
 
