f9adf1f1ea66a335646f2a8f6d857ebb351dcf19
abdellah karani <abdellahkarani@gamil.com>
2024-10-26 19:46:58 +0100

    .

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -4,16 +4,29 @@
         constructor that create an object
         using another object
 
+    - Call to default constructor
+        if we do not write construtor
 
+    - Finalizers vs Destructor
+        used when end to use the object, 
+        we remove space in memory(heap)
+        +Destructor
+            Java does not have destructors like C++
+            Java has garbage collection, which automatically cleans up memory.
+        +Finalizers || finalize method
+            were used for cleanup but are not recommended.
 */
 
-
+/*
 public class App {
     public static void main(String args[]) {
-
+        Product p1 = new Product("M", 12.3f);
+        Product p2 = new Product(p1); // Copy Constructor
+        p2.display();
+        
     }
 }
-
+*/
 
 
 
--- Learn Java/classes/Product.java
+++ Learn Java/classes/Product.java
@@ -2,15 +2,29 @@ public class Product {
     private String name;
     private float price;
 
+    public void display() {
+        System.out.printf("name = %s, price =  %f", this.name, this.price);
+    }
+
+    // No-Arg constructor
     public Product() {
         this.name = "t-shirt";
         this.price = 12.2f;
     }
-    public void display() {
-        System.out.printf("name = %s, price =  %f", this.name, this.price);
-    }
+    // Parameterized constructor
     public Product(String n, float p) {
         this.name = n;
+        this.price = p;
+    }
+    // Copy Constructor
+    public Product(Product obj) {
+        this.name = obj.name;
+        this.price = obj.price;
+        // Another method
+        this (obj.name, obj.price); // call another constructor
+    }
+    // Set price
+    public void setPrice(float p) {
         this.price = p;
     }
 }
