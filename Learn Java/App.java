495533bb38444e78561b58dbd5f68607178ed650
abdellah karani <abdellahkarani@gamil.com>
2024-10-29 18:49:32 +0100

    ,

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,26 +1,13 @@
+// Static keyword (Static Variable Block and Methods)
+
+
 
 public class App {
     public static void main(String[] args) {
-        User user1 = new User();
-        user1.setName("LSLS");
-        user1.setCode(10);
-
-        User user2 = new User("ahmed", 1220);
-
-        user2.display();
-
-        // make a user3 from user2 using copy contractor
-        User user3 = new User(user2);
-
-        user3.display();
-        
-
-
+        Student std = new Student(122, "Abdellah");
+        std.display();
     }
 }
-
-
-// do a small recap and ask moahmed about contarctor default
 
 
 
