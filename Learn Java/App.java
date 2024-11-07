69e9328d2a6ac12f68bacac2fc39ff7dacc3d5d2
abdellah karani <abdellahkarani@gamil.com>
2024-11-07 23:38:14 +0100

    remove learn file

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,4 +1,4 @@
-// Repeat this
+// Relationships (Association, Aggregation "has-a", Composition "part-of")
 
 
 import inheritance.Employee;
@@ -6,18 +6,8 @@ import polymorphism.*;
 
 public class App {
     public static void main(String argv[]) {
-        Animal animal = new Animal();
-        animal.sound();
-        System.out.println("is_arg = " + animal.is_agr());
         
-        Animal animal1 = new Dog();
-        System.out.println("is_arg = " + animal1.is_agr());
-
-        animal1.print();
-        animal1.print1();
-
         
-
 
     }
 }
