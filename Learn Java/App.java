f49118b62ce8a06878b2d654d7e3f1553a50c182
abdellah karani <abdellahkarani@gamil.com>
2024-11-09 19:21:23 +0100

    add relation folder and images for association and start practice for relationsships

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,31 +1,7 @@
-// Relationships (Association, Aggregation "has-a", Composition "part-of")
+// Relationships Practice
 /*
-    Between classes we have: 
-        Association (has-a)
-        inheritance (is-a)
-        composition  (part-of)
-        aggregation (has-a)
+    https://www.youtube.com/watch?v=JXzBz03szvI&list=PLCInYL3l2AajYlZGzU_LVrHdoouf8W6ZN&index=46
     
-    Association: is a connection between two seperate classes
-        that are setup through thier object
-        Unary Association: from A to B
-        Binary Association: from A to B & inverse
-        EX: Personn has-a Address
-        NOTE: in unary association we have three types
-            [general, composition, agregation]
-    
-    Aggregation:
-        ClassA <-- ClassB
-            ClassB is part-of ClassA
-            ClassA has-a ClassB
-        Company <-- Employee
-            if Employee go Company still
-    
-    Composition
-        ClassA <-- ClassB
-            ClassA has no meaning without ClassB
-        Organization <-- Department
-            no Organization no Department
 */
 
 import inheritance.Employee;
@@ -33,8 +9,6 @@ import polymorphism.*;
 
 public class App {
     public static void main(String argv[]) {
-        
-        
 
     }
 }
