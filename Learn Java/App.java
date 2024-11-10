ab5f08a3636fdb3488353ec3b31a5aa66ff51099
abdellah karani <abdellahkarani@gamil.com>
2024-11-10 20:56:24 +0100

    remove classes content for doing later

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,30 +1,3 @@
-// Relationships Practice
-/*
-    delete emp =  delete personnal_info
-    create object in class ---> Composition    
-    pass object to the class ---> Agregation
+// Do Your Own Exemple
+// Do The Binary Association
 
-    - in general Association we don't
-        declare instanceOf other class
-    - No, in a binary association, deleting one 
-        object doesn’t automatically delete the other
-*/
-
-
-import relations.*;
-
-public class App {
-    public static void main(String argv[]) {
-        Department dept = new Department(1001, "safa");
-
-        Employee emp = new Employee(12, 1000, "ahmed", 
-        2005,  "10393903", dept);
-
-        System.out.println(emp.toString());
-        System.out.println(emp); // like emp.toString()
-        
-
-        
-
-    }
-}
