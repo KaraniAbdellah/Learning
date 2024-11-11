75ac085afcf2029131c888587a8dc4fbaf838d6e
abdellah karani <abdellahkarani@gamil.com>
2024-11-11 20:32:37 +0100

    revising again

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,20 +1,7 @@
-/* 
-	task:
-        remove column table
-        
-        drop table & drop database
-*/
 -- start revision
 
 
--- alter table employee -- for make changes
--- add email varchar(40); -- add new column
--- rename column email to phone_number; -- rename column name
--- modify column phone_number text; -- change column type
--- modify first_name text after id; -- change the position
--- modify phone_number text first;
--- drop column last_name; -- drop column
-select * from employee;
+
 
 
 
