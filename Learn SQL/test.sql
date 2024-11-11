d0aa9a82add9b52ea2d13eee356356c0dc37a80b
abdellah karani <abdellahkarani@gamil.com>
2024-11-11 20:48:58 +0100

    reversing tables

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,6 +1,23 @@
 -- start revision
 
 
+alter table worker
+
+-- add column email text; add column
+-- rename column email to phone_number; -- rename column name
+-- modify column phone_number varchar(20); modify column type
+-- modify id int after status_worker; change the position
+-- modify id int first; make id column first
+
+
+
+select * from worker;
+
+
+
+
+
+
 
 
 
