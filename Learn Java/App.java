619a474db2c6dcc5149ffdff2d51176fde62337e
abdellah karani <abdellahkarani@gamil.com>
2024-11-11 20:01:05 +0100

    start revision oop

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,25 +1,12 @@
 
-/*
-    Signle association: like Agregation
-        Developer has a User reference
-    Binary association:
-        each class should have a reference to the other
-    
-    Single Association = Unidirectional
-    Binary Association = Bidirectional
-*/
+// Start Revision OOP
 
 import relations.*;
 
 public class App {
     public static void main(String args[]) {
 
-        // Binary Association
-        Admin admin = new Admin("All", 122, null);
-        User user1 = new User("Ahmed", 10, admin);
-        admin.setUser(user1);
-
-        System.out.println(user1.toString());
+        System.out.println("Starting Revesing");
 
     }
 }
