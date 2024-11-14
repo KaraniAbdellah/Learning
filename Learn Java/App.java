3047b1a29da34018df04c3698e6855abd86fd816
abdellah karani <abdellahkarani@gamil.com>
2024-11-14 19:59:05 +0100

    add Student.java class

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -2,22 +2,33 @@
 /*
     Final Variable (Constance Var)
         can not change the value of final variables
-
-    Final Blank Variable
-
-    Static Final Blank Variable
-
+        always must be initialized with:
+            (constrator || in Declaring || Block)
+        you can not set by (setters || direct access)
+        ----------
+        final static float DEGREE_OF_SUCCESS = 10;
+            shared between all object
+        final float DEGREE_OF_SUCCESS = 10;
+            each object has own DEGREE_OF_SUCCESS
+    
+    static varibales
+        initialize just in (Static Block || in Declaring)
+    
+    Final Blank Varibles
+        we give initilize after Declaring
 
     "sometimes we need to se limitions"
 */
 
 
-import revise.*;
+import final_keyword.*;
 
 public class App {
     public static void main(String args[]) {
         // Final Variable
-        
+        Student s1 = new Student(122, "ahmed");
+        // s1.std_id = 999;
+        // s1.setStd_id(101);
 
     }
 }
--- Learn Java/classes/final_keyword/Student.java
+++ Learn Java/classes/final_keyword/Student.java
@@ -1,5 +1,30 @@
 package final_keyword;
 
 public class Student {
+    private String name;
+    // private final int std_id = 100;
+    final int std_id;
+    Student() {
+        std_id = 10;
+    }
+    // final static float DEGREE_OF_SUCCESS = 10;
+    final static float DEGREE_OF_SUCCESS;
+    static {
+        DEGREE_OF_SUCCESS = 100;
+    }
+
+    public Student(int std_id, String name) {
+        this.std_id = std_id; // must be initialized
+        this.name = name;
+    }
+    public int getStd_id() {
+        return this.std_id;
+    }
+    // public void setStd_id(int age) {
+    //     this.std_id = std_id;
+    // }
+
     
+
+
 }
