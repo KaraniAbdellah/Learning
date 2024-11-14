384758fda5252c01f71ba7d9fc21d84165f72fc7
abdellah karani <abdellahkarani@gamil.com>
2024-11-14 19:15:10 +0100

    finich revesing to today

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -2,16 +2,9 @@
 import revise.*;
 
 public class App {
-    
     public static void main(String args[]) {
-        User user1 = new User("ahmed", 10);
-        user1.setN(10);
-        System.out.println("n = " + user1.getN());
-        user1.setN(incr(user1));
-        System.out.println("n = " + user1.getN());
-
-
-        user1.setN(incr(user1));
+        
+        System.out.println("finich revesing");
 
     }
 }
