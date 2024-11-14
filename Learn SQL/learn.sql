4c8db8a41f93f2162bbecb88ef07222e5c8f49fc
abdellah karani <abdellahkarani@gamil.com>
2024-11-14 21:04:07 +0100

    start learning insert to rows

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -64,6 +64,21 @@ select * from employee;
 
 
 
+-- INSERT ROWS
+/*
+	insert into employee (columnNameToInsertData)
+		values(), (), ..., ();
+*/
+insert into employee 
+values  ("ahmed", "38383", 1030, "ahmed@gmail.com"),
+		("abdellah", "229393", 122, "abdellah@gmail.com"),
+		("omar", "103303", 122, "omar@gmail.com");
+
+insert into employee(first_name, id)
+values ("Ahmed", 110); -- insert just in (first_name, id)
+
+
+
 
 
 
