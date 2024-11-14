66cec68607593a2e02f18bbe8e2764ae7ac81be5
abdellah karani <abdellahkarani@gamil.com>
2024-11-14 21:04:48 +0100

    put search

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,24 +1,5 @@
--- INSERT ROWS
-/*
-	insert into employee (columnNameToInsertData)
-		values(), (), ..., ();
+-- search how to remove row from table in sql
 
-	insert into employee 
-	values  ("ahmed", "38383", 1030, "ahmed@gmail.com"),
-			("abdellah", "229393", 122, "abdellah@gmail.com"),
-			("omar", "103303", 122, "omar@gmail.com");
-
-	insert into employee(first_name, id)
-	values ("Ahmed", 110); -- insert just in (first_name, id)
-
-
-*/
-
-
-
-
-
-select * from employee
 
 
 
