67165eb5d17191e8aaac7a73af9e98b284be04b6
abdellah karani <abdellahkarani@gamil.com>
2024-11-15 20:54:32 +0100

    start reveesing inherstand

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -4,6 +4,6 @@ import final_keyword.*;
 
 public class App {
     public static void main(String args[]) {
-        System.out.println("Start Revesing Inhertance & This Last Lesson");
+        
     }
 }
