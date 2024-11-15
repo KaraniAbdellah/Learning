5f02ca3402c2aeed0fdc7e40577bcb70cf04f411
abdellah karani <abdellahkarani@gamil.com>
2024-11-15 22:33:05 +0100

    learinig select

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -79,6 +79,21 @@ values ("Ahmed", 110); -- insert just in (first_name, id)
 
 
 
+-- Select
+/*
+	select: select the column
+    Where: put condition in selecting
+*/
+
+select * from worker; -- select all column
+select first_name, id from worker; -- select just first_name & id columns
+select * from worker where first_name = "omar"; -- select column thae has name "omar"
+select * from worker where id > 1;
+select * from worker where id != 1;
+select * from worker where id is NULL; -- use "is" not = 
+
+
+
 
 
 
--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,16 +1,9 @@
--- Select
-/*
-	select: select the column
-    Where: put condition in selecting
-*/
+-- Update & Delete
 
-select * from worker; -- select all column
-select first_name, id from worker; -- select just first_name & id columns
-select * from worker where first_name = "omar"; -- select column thae has name "omar"
-select * from worker where id > 1;
-select * from worker where id != 1;
-select * from worker where id is NULL; -- use "is" not = 
 
+update worker set first_name = "abdellah" where email is null;
+
+select * from worker;
 
 
 
