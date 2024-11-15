8aaa2f558ad1f694779c3e786facb15f41460dbb
abdellah karani <abdellahkarani@gamil.com>
2024-11-15 22:24:55 +0100

    learinig select

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,3 +1,16 @@
+-- Select
+/*
+	select: select the column
+    Where: put condition in selecting
+*/
+
+select * from worker; -- select all column
+select first_name, id from worker; -- select just first_name & id columns
+select * from worker where first_name = "omar"; -- select column thae has name "omar"
+select * from worker where id > 1;
+select * from worker where id != 1;
+select * from worker where id is NULL; -- use "is" not = 
+
 
 
 
