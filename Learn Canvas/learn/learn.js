7732ffc745462e81593462c15fb3fe91eb0d6aec
abdellah karani <abdellahkarani@gmail.com>
2024-11-18 22:33:00 +0100

    start learning canvas

---
+++ Learn Canvas/learn/learn.js
@@ -0,0 +1,17 @@
+/* Intro And What Is Canvas */
+/*
+    Canvas is HTML ele allow to draw in browser.
+    if container of the drawing
+*/
+
+
+/* Canvas In Deep and Real Life Examples */
+/*
+    [1]: <canvas width="200" height="200" id="canva"></canvas>
+    [2]: can be style with css but just the canvas container
+        canvas {
+            border: 10px blue solid;
+        }
+    [3]: Default Demension [300, 150, transparent]
+*/
+
---
+++ Learn Canvas/test/main.js
@@ -0,0 +1,1 @@
+// Start Wrtting code
