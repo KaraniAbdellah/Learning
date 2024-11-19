ea516d2c57ecd8579b7728503d97387bd864151c
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 10:29:49 +0100

    start learn copy contractor

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,8 +1,17 @@
 // ArrayList
 /*
     Declaration
+        // size = 10 byDefault
+        ArrayList<Integer> nums = new ArrayList<>();
     Print
+        System.out.println(nums + "\n--------");
     Methods(Add, Get, AddAll, Copy Constructor, Clone) 
+        - add(ele);
+        - ArrayListName.size();
+        - ArrayListName.get(index);
+        - ArrayListName.addAll(ArrayListName);
+        - ArrayListName.addAll(Arrays.asList(1, 2, 3, 4));
+        - ArrayListName.addAll(index, Arrays.asList(1, 2, 3, 4));
 */
 
 import java.util.*;
@@ -12,23 +21,36 @@ import revise.*;
 public class App {
     public static void main(String args[]) {
         // Declaration
-        ArrayList<Integer> nums = new ArrayList<>(); // size = 10 byDefault
+        ArrayList<Integer> nums = new ArrayList<>();
         ArrayList<String> names = new ArrayList<String>();
 
         // Methods
-        nums.add(10);
-        nums.add(230);
-        nums.add(0, 1001); // shift 10 to right
+        nums.add(1);
+        nums.add(2);
+        nums.add(0, 0); // shift 10 to right
+        nums.add(nums.size(), 3);
+
+        // add range of eles
+        ArrayList<Integer> nums1 = new ArrayList<Integer>();
+        nums1.add(-1);
+        nums1.addAll(nums);
+        nums1.addAll(Arrays.asList(1, 2, 3));
+        nums1.addAll(3, Arrays.asList(5, 5, 5));
 
         // Print
-        System.out.println(nums);
+        System.out.println("nums = " + nums);
+        System.out.println("nums1 = " + nums1);
+        System.out.println("-------");
         int s = nums.size();
         for (int i = 0; i < s; i++) {
             System.out.println(nums.get(i));
-        }
+        } System.out.println("--------");
         for (int num: nums) {
             System.err.println(num);
         }
+
+        // Copy Constructor
+        
 
 
     }
