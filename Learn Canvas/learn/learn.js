a26d07c839563cef0e00609f4e31214302afb0a3
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 14:57:35 +0100

    select pattern

--- Learn Canvas/learn/learn.js
+++ Learn Canvas/learn/learn.js
@@ -17,6 +17,7 @@
 
 
 
+
 // Context And Filling Style
 /*
     By Default Context Color in Black
@@ -32,6 +33,34 @@ theContext.fillStyle = "red"; // Color(#F00), Gradiant, Pattern
 
 // Draw Rectangle
 theContext.fillRect(0, 0, 100, 100);
+theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+
+
+
+// Gradients and Pattern Fill 
+/*
+    createLinearGradient(x0, y0, x1, y1);
+        x0 --> start from left
+        y0 --> start from top
+        x1 --> end from left
+        y1 --> end from top
+*/
+
+theCanvas = document.querySelector("#our-canvas"),
+theContext = theCanvas.getContext("2d"),
+
+// Select Gradient
+theGradiant = theContext.createLinearGradient(0, 1, 0, 100); 
+theGradiant.addColorStop(0, "red");
+theGradiant.addColorStop(1, "blue");
+theGradiant.addColorStop(1, "yellow");
+// Select Pattern
+let theImage = document.querySelector(".pattern");
+thePattern = theContext.createPattern(theImage, "no-repeat"); // repeat, repeat-x, repeat-y
+
+theContext.fillStyle = thePattern;
+theContext.fillStyle = theGradiant;
 theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
 
 
--- Learn Canvas/test/main.js
+++ Learn Canvas/test/main.js
@@ -1,20 +1,15 @@
 // Gradients and Pattern Fill 
-/*
-    createLinearGradient(x0, y0, x1, y1);
-        x0 --> start from left
-        y0 --> start from top
-        x1 --> end from left
-        y1 --> end from top
-*/
+
 
 let theCanvas = document.querySelector("#our-canvas"),
-theContext = theCanvas.getContext("2d"),
-theGradiant = theContext.createLinearGradient(0, 1, 0, 100); 
-theGradiant.addColorStop(0, "red");
-theGradiant.addColorStop(1, "blue");
-theContext.fillStyle = theGradiant;
+theContext = theCanvas.getContext("2d");
+
+// Select Pattern
+let theImage = document.querySelector(".pattern");
+
+// Create Pattern
+thePattern = theContext.createPattern(theImage, "no-repeat"); // repeat, repeat-x, repeat-y
+
+theContext.fillStyle = thePattern;
 theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
-
-
-
 
