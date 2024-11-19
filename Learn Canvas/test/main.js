4669e11f466d2a5a3739cfba46e278b507f77947
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 14:58:10 +0100

    set a task

--- Learn Canvas/test/main.js
+++ Learn Canvas/test/main.js
@@ -1,15 +1,1 @@
-// Gradients and Pattern Fill 
-
-
-let theCanvas = document.querySelector("#our-canvas"),
-theContext = theCanvas.getContext("2d");
-
-// Select Pattern
-let theImage = document.querySelector(".pattern");
-
-// Create Pattern
-thePattern = theContext.createPattern(theImage, "no-repeat"); // repeat, repeat-x, repeat-y
-
-theContext.fillStyle = thePattern;
-theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
-
+// select gradient and pattern
