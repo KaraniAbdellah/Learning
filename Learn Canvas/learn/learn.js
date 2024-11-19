10b5b9bdd6064954855e5ec097a20490ff9dc582
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 14:21:28 +0100

    add first lessosn

--- Learn Canvas/learn/learn.js
+++ Learn Canvas/learn/learn.js
@@ -15,3 +15,20 @@
     [3]: Default Demension [300, 150, transparent]
 */
 
+
+
+// Context And Filling Style
+// select the canvas element
+let theCanvas = document.querySelector("#our-canvas");
+
+// select context type
+theContext = theCanvas.getContext("2d");
+
+// choose fill style
+theContext.fillStyle = "#F00"; // Coloe, Gradiant, Pattern
+
+// Draw Rectangle
+theContext.fillRect(0, 0, 100, 100);
+theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+
--- Learn Canvas/test/main.js
+++ Learn Canvas/test/main.js
@@ -1,1 +1,16 @@
-// Start Wrtting code
+// Context And Filling Style
+// select the canvas element
+let theCanvas = document.querySelector("#our-canvas");
+
+// select context type
+theContext = theCanvas.getContext("2d");
+
+// choose fill style
+theContext.fillStyle = "#F00"; // Coloe, Gradiant, Pattern
+
+// Draw Rectangle
+theContext.fillRect(0, 0, 100, 100);
+theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
+
+
