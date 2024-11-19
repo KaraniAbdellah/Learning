3d1e019a0fea42619fad8a6e8071eb5ba4f8308c
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 21:12:57 +0100

    add stroke

--- Learn Canvas/learn/learn.js
+++ Learn Canvas/learn/learn.js
@@ -64,3 +64,29 @@ theContext.fillStyle = theGradiant;
 theContext.fillRect(0, 0, theCanvas.width, theCanvas.height);
 
 
+
+
+// StrokeRect And Text
+/*
+    Stroke do just the Border
+*/
+
+theCanvas = document.querySelector("#our-canvas");
+    theContext = theCanvas.getContext("2d");
+    theGradient = theContext.createLinearGradient(0, 0, 200, 0);
+    theGradient.addColorStop(0, "red");
+    theGradient.addColorStop(1, "yellow");
+
+theContext.lineWidth = 2; // border = 10px
+theContext.strokeStyle = theGradient;
+theContext.strokeRect(10, 10, 100, 100);
+
+// Set font & Create filled text
+theContext.font = "20px Arial";
+// theContext.fillStyle = theGradient;
+// theContext.fillText("Hello", 40, 60);
+theContext.strokeStyle = theGradient;
+theContext.strokeText("Hello", 40, 60);
+
+
+
--- Learn Canvas/test/main.js
+++ Learn Canvas/test/main.js
@@ -1,1 +1,26 @@
-// select gradient and pattern
+// StrokeRect And Text
+/*
+    Stroke do just the Border
+*/
+
+let theCanvas = document.querySelector("#our-canvas");
+    theContext = theCanvas.getContext("2d");
+    theGradient = theContext.createLinearGradient(0, 0, 200, 0);
+    theGradient.addColorStop(0, "red");
+    theGradient.addColorStop(1, "yellow");
+
+theContext.lineWidth = 2; // border = 10px
+theContext.strokeStyle = theGradient;
+theContext.strokeRect(10, 10, 100, 100);
+
+// Set font & Create filled text
+theContext.font = "20px Arial";
+// theContext.fillStyle = theGradient;
+// theContext.fillText("Hello", 40, 60);
+theContext.strokeStyle = theGradient;
+theContext.strokeText("Hello", 40, 60);
+
+
+
+
+
