caf491f134f12a600942448ba75533d8a8664d92
abdellah karani <abdellahkarani@gmail.com>
2024-11-19 13:05:55 +0100

    start revesing rollback

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,3 +1,13 @@
+BEGIN;
 
+
+-- Drop the table
+DROP TABLE test;
+
+-- Rollback to undo the drop
+ROLLBACK;
+
+-- Check if the table exists
+SELECT * FROM test; -- The table and data are still there after rollback
 
 
