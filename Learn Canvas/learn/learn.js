d304c581072f4c21b6bf10b17b408235d794e733
abdellah karani <abdellahkarani@gmail.com>
2024-11-20 23:00:30 +0100

    start learning path and line

--- Learn Canvas/learn/learn.js
+++ Learn Canvas/learn/learn.js
@@ -115,3 +115,27 @@ theCanvas = document.querySelector("#our-canvas");
 
 
 
+
+// Paths And Lines
+/*
+    The Base of All Things
+*/
+theCanvas = document.querySelector("#our-canvas");
+theContext = theCanvas.getContext("2d");
+
+// Begin Path
+theContext.beginPath();
+// Select The Start Position
+theContext.moveTo(10, 10);
+// Select The Path Point
+theContext.lineTo(100, 10);
+
+// Adjust Style and Line Width
+theContext.lineWidth = 10;
+theContext.strokeStyle = "grey";
+
+// Draw The Path
+theContext.stroke();
+    
+
+
