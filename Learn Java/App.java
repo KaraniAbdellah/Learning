b976b3e9d57cb79e326c538a5fb0a17f88bf1b2f
abdellah karani <abdellahkarani@gmail.com>
2024-11-20 21:49:59 +0100

    start revesing ArrayList

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -5,7 +5,21 @@ import revise.*;
 
 public class App {
     public static void main(String args[]) {
-        System.out.println("revise Array List");
+        // Copy Constractor || Shallow Copy
+        ArrayList <Integer>marks = new ArrayList<Integer>();
+        marks.add(1);
+        ArrayList <Integer>notes = new ArrayList(marks);
+        marks.add(22);
+        System.out.println("notes = " + notes);
+        System.out.println("marks = " + marks);
+        
+        // Clone Constractor || Reference Copy
+        marks = notes;
+        marks.add(999);
+        System.out.println("notes = " + notes);
+        System.out.println("marks = " + marks);
+
+
     }
 }
 
