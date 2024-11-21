931954e80e9a609b8e26d2ca852fc449b0da00fd
abdellah karani <abdellahkarani@gmail.com>
2024-11-21 22:27:02 +0100

    Revise SQL

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,13 +1,1 @@
--- Auto Increment
-/*
-	in each row we are going to increment the column value
-*/
-
-create table transactions(
-	transactions_id int primary key auto_increment,
-    transactions_name text
-);
-
-insert into transactions(transactions_name) values ("XXX");
-select * from transactions;
-
+-- revise SQL and do 10min
