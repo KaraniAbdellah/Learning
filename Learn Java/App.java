92e9acfab2e4040517fe9c3d7f7779d70740fba1
abdellah karani <abdellahkarani@gmail.com>
2024-11-21 21:44:14 +0100

    start arrayList

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,44 +1,13 @@
 // ArrayList Methods
 
-/* 
 import java.lang.reflect.Array;
 import java.util.*;
 
 public class App {
     public static void main(String args[]) {
-        ArrayList <Integer>nums = new ArrayList(Arrays.asList(1, 2));
-        ArrayList <Integer>notes = new 
-        ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 2, 3, 4)); 
-        
-        // contains
-        boolean check = nums.contains((Integer) 0);
-        System.err.println("check = " + check);
-
-        // retainAll
-        notes.retainAll(nums);
-        System.out.println(notes);
-
-        // subList
-        ArrayList <Integer>nums_r = new ArrayList<>
-        (notes.subList(0, 2));
-        System.out.println(nums_r); // [1, 1]
-
-        // clear
-        boolean check_empty = notes.isEmpty();
-        notes.clear();
-        System.out.println("check_empty = " + check_empty + " notes = " + notes);
-        
-        // ensureCapacity & trimToSize
-        nums.ensureCapacity(100);
-        nums.trimToSize();
-
-        // forEach && sort
-        nums.forEach(n->System.out.println(n));
-        Collections.sort(nums, Collections.reverseOrder()); 
-        System.out.println("nums = " + nums);
+        System.out.println("Hello World");
     }
 }
-*/
 
 
 
