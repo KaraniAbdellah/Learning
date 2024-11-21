b41bd8f895f76cb8e9646ce9562bb994280e60fd
abdellah karani <abdellahkarani@gmail.com>
2024-11-21 22:04:57 +0100

    Revise SQL

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,6 +1,12 @@
--- revise the previes lesseons
 
 
+-- select * from products;
+-- select * from products where id = 0;
+-- select * from products where product_name is not null;
+
+-- insert into products values (10, "X-phone", 12.2);
+-- update products set product_name = "YYY", product_price = 0.01 where product_name = "YYY";
+delete from products where product_name = "YYY";
 select * from products;
 
 
