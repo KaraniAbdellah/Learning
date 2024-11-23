0b2d75cc2f8ba3956dc54c6ffbc4d77325aca35d
abdellah karani <abdellahkarani@gmail.com>
2024-11-23 21:53:26 +0100

    create a clients table

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -4,6 +4,27 @@
 		--> we can link between two tables
         --> we can access to table 2 by table 1
 */
+/*
+create table clients(
+	client_id int primary key auto_increment,
+    client_name varchar(30)
+);
+*/
+-- insert into clients(client_name) values ("ali");
+
+
+select * from clients;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
