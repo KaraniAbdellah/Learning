600740d1551d02ddbe457f8695a22519c7c08b68
abdellah karani <abdellahkarani@gmail.com>
2024-11-23 21:43:14 +0100

    revise SQL lessons (constraint)

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,1 +1,15 @@
--- revise SQL and do 10min
+/*
+create table workers (
+	first_name varchar(20) unique,
+    last_name varchar(10) not null,
+    id int default 10,
+    salary float default 120,
+    constraint salary_checker check (salary > 100)
+);
+*/
+
+insert into workers(first_name, last_name, salary) values ("s", null, 12);
+
+select * from workers;
+
+
