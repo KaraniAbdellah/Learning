6650c2274e2b894a5e7ebb68ac09987b59651643
abdellah karani <abdellahkarani@gmail.com>
2024-11-23 22:57:47 +0100

    add exercice 2

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,20 +1,46 @@
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class App {
+    static void Menu() {
+        System.out.println("1. Add Element");
+        System.out.println("2. Remove Element");
+        System.out.println("3. Print Element");
+        System.out.println("4. Exit");
+    }
+    static void add_ele(ArrayList nums) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int ele = in.nextInt();
+        nums.add((Integer) ele);
+    }
+    static void remove_ele(ArrayList nums) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int ele = in.nextInt();
+        nums.remove((Integer) ele);
+    }
+    static void print_ele(ArrayList nums) {
+        System.out.println(nums);
+    }
     public static void main(String args[]) {
         ArrayList <Integer>nums = new ArrayList<Integer>(5);
         Scanner in = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("%d: number = ", i);
-            int n = in.nextInt();
-            System.out.println(n);
-            nums.add(n);
-            System.out.println(nums);
-            if (nums.contains((Integer) n) == true) {
-                nums.remove((Integer) n);
-            }
-        }
+        int choise;
+        do {
+            Menu();
+            System.out.print("Enter You Choice: ");
+            choise = in.nextInt();
+            if (choise == 1) {
+                add_ele(nums);
+            } else if (choise == 2) {
+                remove_ele(nums);
+            } else if (choise == 3) {
+                print_ele(nums);
+            }else;
+        } while (choise != 4);
+
         
     }
 }
