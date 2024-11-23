1fa3a72770e6fc8e775702ea71c91b9ee5e5b4a6
abdellah karani <abdellahkarani@gmail.com>
2024-11-23 22:45:41 +0100

    start exercices

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,11 +1,21 @@
-// Do the Exercice in Java & Finich the Other Videos
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class App {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        ArrayList <Integer>nums = new ArrayList<Integer>(5);
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("%d: number = ", i);
+            int n = in.nextInt();
+            System.out.println(n);
+            nums.add(n);
+            System.out.println(nums);
+            if (nums.contains((Integer) n) == true) {
+                nums.remove((Integer) n);
+            }
+        }
+        
     }
 }
 
