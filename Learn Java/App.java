4d0db8c137cb5691bde49e688ad6d9902fdfe790
abdellah karani <abdellahkarani@gmail.com>
2024-11-24 23:00:23 +0100

    take intro to abstract

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,26 +1,5 @@
 // try to revise all thing (fast revise)
 
-////////////////////////////////////////////////////////////
-/*
-    Abstraction: hiding the internal implementation(method,feature)
-        and only showing the functionality to the users.
-    
-    We achieve Abstraction By:
-        Abstract Class
-        Interface
-    
-    Abstract Class:
-        - non-access modifiers
-        - Class declared with abstract keyword is known as an abstract
-            class in java. (can have abstrcat || no abstrcat method)
-        - Allow you to create blueprints for concrete classes
-        - Concrete class: is a class that has an implmentation
-            for all methods (that are abstracted)
-        - NOTE: we define just the prototype, the Method
-            will be implmented in Classes.
-        - cannot be instantiated
-        - can we have constrcator & static & final (Methods)
-*/
 
 public class App {
     public static void main(String args[]) {
