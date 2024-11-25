986966854affde2dc26e13197d66b7622d4d8844
abdellah karani <abdellahkarani@gmail.com>
2024-11-25 18:22:06 +0100

    create Classes car.java ....

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,32 +1,10 @@
 // try to revise all thing (fast revise)
+// try to revise abstract class
 
-/*
-    Abstract Class: can have abstract and non-abstract methods
-    Concrete Class: is class that has implementation for methods
-    Concrete Method: is method has body
-
-    We can not define object with abstract Class
-
-    - Rules of asbtract class:
-        We do not have 
-            abstract varaible
-            abstract contractor
-            abstract with static Methods
-        private abstract void autopilot(); --> error
-*/
 import abstraction.*;
 public class App {
     public static void main(String args[]) {
-        SUV suv1 = new SUV();
-        suv1.autopilot(); // Hello AutoPilot
-        suv1.sayHello(); // Hello
-
-        // Car car2 = new Car(10, 10, 10); // Cannot instantiate the type Car
-
-        // Define SUV object with Contractor of abstract Class
-        SUV suv2 = new SUV(12, 23, 4);
-        suv2.streamingServices();
-
+       
     }
 }
 
