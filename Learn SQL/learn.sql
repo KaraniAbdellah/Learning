5fa132568153ac1f2cde3aa6643c28c6dee4cd2c
abdellah karani <abdellahkarani@gmail.com>
2024-11-25 15:01:23 +0100

    start my own exemple of limit clause

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -388,6 +388,23 @@ select * from employees order by last_name ASC, first_name ASC;
 
 
 
+-- LIMIT CLAUSE
+/*
+	used to limit the number of records
+    used to display a large data on pages (pagination)
+    
+    3, 1: skip the first row and get the next three rows.
+    2, 3:  skip the first two rows and get the next three rows.
+    x, y: skip x rows and get next y rows.
+*/
+
+select * from employees limit 1; -- get just the first employees
+select * from employees order by first_name asc limit 2; 
+select * from employees limit 2, 3;
+
+
+
+
 
 
 
--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,4 +1,16 @@
+-- LIMIT CLAUSE
+/*
+	used to limit the number of records
+    used to display a large data on pages (pagination)
+    
+    3, 1: skip the first row and get the next three rows.
+    2, 3:  skip the first two rows and get the next three rows.
+    x, y: skip x rows and get next y rows.
+*/
 
+select * from employees limit 1; -- get just the first employees
+select * from employees order by first_name asc limit 2; 
+select * from employees limit 2, 3;
 
 
 
