804cc71bf866187cd4cad168d3d5e247d9da2353
abdellah karani <abdellahkarani@gmail.com>
2024-11-25 15:41:39 +0100

    start asbtract class

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,11 +1,16 @@
 // try to revise all thing (fast revise)
 
+// Abstract Class part 1 (Examples, Abstract rules)
+/*
+    Abstract Class: can have abstract and non-abstract methods
+    Concrete Class: is class that has implementation for methods
+    Concrete Method: is method has body
+*/
 
 public class App {
     public static void main(String args[]) {
-        System.out.println("Start Abstraction");
+        
     }
-
 }
 
 
