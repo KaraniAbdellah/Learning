e5e8478e93deca71f84fa45e1b81ac2b14e87522
abdellah karani <abdellahkarani@gmail.com>
2024-11-25 13:50:41 +0100

    create employees & compte table

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,3 +1,26 @@
+/*
+create table employees (
+	id_emp int primary key auto_increment,
+    first_name varchar(25) default "John",
+    last_name varchar(25) default "Deo"
+);
+create table compte (
+	id_cmp int primary key auto_increment,
+    amount int default 0,
+    id_emp_cmt int,
+    foreign key (id_emp_cmt) references employees (id_emp)
+);
+*/
+
+
+
+
+
+
+
+
+
+
 
 
 
