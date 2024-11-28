0cf868c6fb4aa219ca4a7a891b34078fe55f92d0
abdellah karani <abdellahkarani@gmail.com>
2024-11-28 22:51:26 +0100

    start interface

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,12 +1,8 @@
 
-import revise.abstract_classes.*;
 
 public class App {
     public static void main(String args[]) {
-        Admin admin1 = new Admin(12, "Ahmed");
-        admin1.sayId();
-        admin1.sayUser();
+        // start interface
     }
 }
-
 
