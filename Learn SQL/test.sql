d875f68a7ac75d6697964095b56d2982413006ec
abdellah karani <abdellahkarani@gmail.com>
2024-11-30 16:48:07 +0100

    start learning views

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,17 +1,19 @@
 -- SELF JOINS
 /*
 	- join the same table 
-    
 */
-
-/*
 select emp1.first_name, emp1.last_name,
 		emp2.first_name, emp2.last_name
 from employees as emp1 
 inner join 
 employees as emp2 
 on emp1.referal_id = emp2.id;
-*/
+
+
+
+
+
+
 
 
 
