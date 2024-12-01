7c6e7a21fe0a2d066071cec1da817608b3a004b7
abdellah karani <abdellahkarani@gmail.com>
2024-12-01 21:05:59 +0100

    start interface

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,8 +1,10 @@
-/// Start Practice Interface
+// Start Practice Interface
+
 
 public class App {
     public static void main(String args[]) {
         System.out.println("Start Interface");
+        
     }
 }
 
