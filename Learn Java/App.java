de0ec34e962fc4c2d3d38f1c1760834b560eff74
abdellah karani <abdellahkarani@gmail.com>
2024-12-02 16:07:15 +0100

    add def between interfaces and abstract class

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -5,6 +5,23 @@
     By default Attributes in interface in 
         public static final --> "must give to him a init value"
 */
+/* 
+    Abstract Class:
+
+    Can inherit from a regular (non-abstract) class.
+    Can have methods with or without a body (partially implemented).
+    Can have attributes and constructors.
+    Supports single inheritance (one abstract class at a time).
+
+
+    Interface:
+    Cannot inherit from a regular (non-abstract) class. 
+        It can only extend other interfaces.
+    All methods are abstract by default (before Java 8).
+    No attributes (only constants allowed).
+    Supports multiple inheritance (a class can
+        implement manyinterfaces).
+*/
 // revise the interfaces
 
 import abstraction_two.CarV1;
