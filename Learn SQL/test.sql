7fadedf24f480c32efbcbbbb564025fb0ebabc4e
abdellah karani <abdellahkarani@gmail.com>
2024-12-03 19:42:09 +0100

    create student_score table

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,10 +1,15 @@
 -- start TDS
 
 
-select * from employees
+select * from employees;
 select * from students;
+INSERT INTO student_scores (notes, marks) VALUES
+('Good performance', 85),
+('Needs improvement', 60),
+('Excellent work', 95);
 
-select * from employees join students;
+
+select * from employees join students join employees;
 
 
 
