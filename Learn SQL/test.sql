d8c7a38fbb4bc58562aee3b587a18b3f04b2f23e
abdellah karani <abdellahkarani@gmail.com>
2024-12-03 19:48:49 +0100

    add tas

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,11 +1,1 @@
--- start TDS
-
-
-select * from employees;
-select * from students;
-select * from student_scores;
-
-select * from employees join students join student_scores where id = 1;
-
-select * from employees where referal_id > 10 or (last_name = "ahmed" and referal_id = 3);
-
+-- revise all this please, and finich TD 3
