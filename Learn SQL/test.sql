c5e36d8d1d37ce78e6b1d56e14bd5f3fae52dd16
abdellah karani <abdellahkarani@gmail.com>
2024-12-03 13:20:33 +0100

    add comments

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,4 +1,9 @@
--- SUBQUERIES
+-- | Nested queries - Sub queries | - |Any , All |
+/*
+	Nested queries || Sub queries: is querie inside another querie
+	nested query (inside query) runs first
+*/
 
+select * from employees where id = (select max(id) from employees);
 
 
