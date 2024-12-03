d2d8863064032db707cdc8c341a04e6e61f7da62
abdellah karani <abdellahkarani@gmail.com>
2024-12-03 19:23:29 +0100

    submit all

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -489,6 +489,38 @@ select * from employees where first_name = "abdellah"; -- fast search
 
 
 
+-- | Nested queries - Sub queries | - |Any , All |
+/*
+	Nested queries || Sub queries: is querie inside another querie
+	nested query (inside query) runs first After the outer query
+    
+    ALL is used to compare a value with all results from the inner query
+    ANY is for comparing with any one result
+*/
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+INSERT INTO students (name, last_name) VALUES
+('John', 'Doe'),
+('Jane', 'Smith'),
+('Michael', 'Johnson');
+
+select * from employees where id = (select max(id) from employees);
+select * from employees where id > (select avg(id) from employees);
+
+-- get the ids bigger then all students id
+select * from employees where id > all (select id from students);
+
+-- get the ids bigger then any students id
+select * from employees where id > any (select id from students);
+
+
+
+
+
 
 
  
