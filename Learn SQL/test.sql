199cc2fbdc2c085e1dbacfa3d9b61a39127b9c0b
abdellah karani <abdellahkarani@gmail.com>
2024-12-03 13:29:30 +0100

    create a student table

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,9 +1,23 @@
 -- | Nested queries - Sub queries | - |Any , All |
 /*
 	Nested queries || Sub queries: is querie inside another querie
-	nested query (inside query) runs first
+	nested query (inside query) runs first After the outer query
 */
 
-select * from employees where id = (select max(id) from employees);
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+INSERT INTO students (name, last_name) VALUES
+('John', 'Doe'),
+('Jane', 'Smith'),
+('Michael', 'Johnson');
+
+
+-- select * from employees where id = (select max(id) from employees);
+-- select * from employees where id > (select avg(id) from employees);
+
+select * from students;
 
 
