380fa3bb1158a75e0b3f8622e843e1dd7dbe1362
abdellah karani <abdellahkarani@gmail.com>
2024-12-04 22:22:14 +0100

    start on delete class

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -546,6 +546,23 @@ having count(amount) > 2;
 
 
 
+-- ROLLUP
+/*
+	ROLLUP: extension of GROUP BY
+    used to add extra rows
+        
+    WITH ROLLUP
+*/
+select * from transactions;
+select sum(amount), order_date from transactions 
+group by order_date with rollup; -- add row for sum of amount rows
+
+select count(amount), order_date from transactions 
+group by order_date with rollup; 
+
+
+
+
 
  
 
