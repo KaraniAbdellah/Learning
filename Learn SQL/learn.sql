c974a5d4ef0e8f75d161a4890a7dba689fd78e64
abdellah karani <abdellahkarani@gmail.com>
2024-12-04 22:12:04 +0100

    start learn group by & having

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -521,6 +521,30 @@ select * from employees where id > any (select id from students);
 
 
 
+-- Group BY
+/*
+	GROUP BY in SQL groups rows with the same values in specified columns.
+	It's used to apply aggregate functions like COUNT() or SUM() to each group.
+	
+    inseated of using WHERE use HAVING
+*/
+
+create table transactions(
+	transaction_id int primary key auto_increment, 
+    amount float,
+    customer_id int, 
+    order_date date
+);
+
+select * from transactions order by order_date asc;
+select sum(amount), order_date from transactions group by order_date;
+select count(amount), order_date 
+from transactions
+group by order_date
+having count(amount) > 2;
+
+
+
 
 
  
