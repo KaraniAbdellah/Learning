33df019539b25c844ab20ad313a342c60226b665
abdellah karani <abdellahkarani@gmail.com>
2024-12-04 22:38:04 +0100

    add to learn.java

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,23 +1,9 @@
-// Start Practice Interface
-/*
-    - class can implement multiple interfaces [not extend]
-    - interface can extend an multiple interfaces
-    - By default Attributes in interface in 
-        public static final --> "must give to him a init value"
-*/
+// try to revise interface & abstract method with your own example
 
-import abstraction_two.*;
 
 public class App {
     public static void main(String args[]) {
-        CarV1 C1 = new CarV1(1, 2, 100);
-        C1.destination("10102939");
-
-        // Reasibilty
-        SelfDrivale car = new CarV1(0, 0, 0);
-        car.drive(); // drive... V1
-        car = new CarV2(0, 0, 0);
-        car.drive(); // drive... V2
+        System.out.println("Revesing...");
     }
 }
 
