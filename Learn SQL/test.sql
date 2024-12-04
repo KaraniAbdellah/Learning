e10bc9f247655f23f0103cdeda67a5936637f4db
abdellah karani <abdellahkarani@gmail.com>
2024-12-04 19:30:44 +0100

    create 4 table

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,6 +1,33 @@
+create table Developpeur(
+	NumDev varchar(20),
+    NomDev int,
+    AdrDev text, 
+    EmailDev varchar(30),
+    TelDev varchar(20)
+);
+
+create table Projet (
+	NumProj varchar(20),
+    TitreProj varchar(20),
+    DateDeb Date,
+    DateFin Date
+);
+
+create table Logiciel (
+	CodeLog int,
+    NomLog text,
+    PrixLog float,
+    NumProj varchar(20),
+    foreign key(NumProj) references Projet (NumProj)
+);
+
+create table Realisation (
+	NumProj varchar(20),
+    NumDev varchar(20),
+    foreign key(NumProj) references Projet (NumProj),
+    foreign key(NumDev) references Developpeur (NumDev)
+);
 
 
-drop table employees;
-drop table students;
-drop table student_scores;
+
 
