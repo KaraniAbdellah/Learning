d1a6161f63d8ce7355d31f5822bbb10714dd2511
abdellah karani <abdellahkarani@gmail.com>
2024-12-05 21:51:07 +0100

    start last part of interface

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -4,9 +4,13 @@
         empty interface
 */
 
+import java.util.*;
+
 public class App {
     public static void main(String args[]) {
         System.out.println("Revesing...");
+        List list = new ArrayList();
+
     }
 }
 
