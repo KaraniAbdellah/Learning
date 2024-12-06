2503801a53a66c2e4af333b467376a489cf31264
abdellah karani <abdellahkarani@gmail.com>
2024-12-06 22:07:55 +0100

    on delete foreign key

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -2,6 +2,10 @@
 /*
 	On delete Set Null: When Foriegn Key Deleted --> repelace it by NULL
 	On delete Set Cascade: When Foriegn Key Deleted --> delete row
+    
+    we are going to delete form customers
+		- will replace foreign key by null
+        - delete the rows from transactions
 */
 
 /*
@@ -16,6 +20,39 @@ create table transactions(
     customer_id int, 
     amount float,
 	foreign key(customer_id) references customers (customer_id)
+    on delete set null -- if we delete foreign key will replaced by null
 );
 */
+-- insert into customers(customer_id, first_name, last_name) values(3, "toto", "mohamed");
+-- insert into transactions(customer_id, amount) values(3, 303);
+-- delete from customers where customer_id = 2; -- can not delete ---> this is forein key
+
+-- add clause ON DELETE SET NULL
+/* 
+alter table transactions add constraint fk_customer_id
+foreign key(customer_id) references customers (customer_id)
+on delete set null;
+*/
+
+-- drop foriegn key
+/*
+alter table transactions drop foreign key fk_customer_id;
+*/
+
+delete from customers where customer_id = 2; -- foreign will replace by null
+delete from transactions where customer_id = 3; -- no problem here
+
+select * from customers;
+select * from transactions;
+
+
+
+
+
+
+
+
+
+
+
 
