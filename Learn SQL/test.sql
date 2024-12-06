4385421c5f795f54d6047f3df99a7fe33c17b600
abdellah karani <abdellahkarani@gmail.com>
2024-12-06 21:43:05 +0100

    create transactions & customers tables

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -4,7 +4,18 @@
 	On delete Set Cascade: When Foriegn Key Deleted --> delete row
 */
 
+/*
+create table customers (
+	customer_id int primary key, 
+    first_name varchar(20),
+    last_name varchar(20)
+);
 
-
-
+create table transactions(
+	tran_id int primary key auto_increment,
+    customer_id int, 
+    amount float,
+	foreign key(customer_id) references customers (customer_id)
+);
+*/
 
