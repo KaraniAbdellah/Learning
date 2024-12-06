646c871af07e10432596788e15a0c1d332d14528
abdellah karani <abdellahkarani@gmail.com>
2024-12-06 22:40:47 +0100

    start Anonymous Inner Clas

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,27 +1,4 @@
-// Other Concept about Interface
-
-/* 
-import abstraction_two.*;
-public class App {
-    public static void main(String args[]) {
-        // Marker Interface
-        Admin admin = new Admin();
-        if (admin instanceof Marker) {
-            System.out.println("YES!!");
-        }
-        else System.out.println("No!!");
-
-        // Generic Interface Without Casting
-        Generic UserObj = new User();
-        // User user = (User) UserObj.GetUser(); error --> must has a type
-        Object user2 = UserObj.GetUser(); 
-
-        // Generic Interface With Casting
-        Generic <User> UserObj1 = new User();
-        User user4 = (User) UserObj1.GetUser();
-        User user5 = UserObj1.GetUser();
-        Object user6 = UserObj1.GetUser();
-    }
-}
+// Anonymous Inner Class
+/*
+    make a code more concise(brief)
 */
-
