1be1711c9bc38c68d29acf33124d5600d1f3de05
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 21:47:12 +0100

    start triggers

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,32 +1,7 @@
--- STORED PROCEDURES
+-- Triggers
 /*
-	you can save query if you are using query a lot of time
+	Trigger = When an event happens, do something
 */
-
--- Example without argument
-delimiter $$
-create procedure get_customers()
-begin 
-	select * from transactions;
-end $$
-delimiter ;
-drop procedure get_customers;
-call get_customers();
-
--- Example with argument
-delimiter $$
-create procedure get_transaction(in number_column int, in id int)
-begin 
-	select * from transactions where customer_id = id limit number_column ;
-end $$
-delimiter ;
-call get_transaction(2, 2);
-
-
-
-
-
-
 
 
 
