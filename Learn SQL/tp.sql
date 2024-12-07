0f860bae73644cd3f1a0882dd56f6dc749c8d078
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 11:04:08 +0100

    add new file

--- Learn SQL/tp.sql
+++ Learn SQL/tp.sql
@@ -1,8 +1,31 @@
+-- TP 1
+/*
+	must data "csv" format
+*/
+
+/*
 create database tp_test;
 use tp_test;
-
 create table data_tp(
-	
+	date_naissance varchar(20),
+    SPV double, 
+    GLD double, 
+    AMAZON double,
+    GOOGLE double,
+    KPTI double,
+    GILD double,
+    MPC double
 );
+*/
+
+
+select * from data_csv;
+
+
+
+
+
+
+
 
 
