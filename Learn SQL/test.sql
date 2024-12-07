502b5672eda45c7a376a49aef413e451a805b64a
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 21:45:11 +0100

    create new procedure

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -21,5 +21,21 @@ call get_customers();
 
 -- Example with argument
 
+delimiter $$
+create procedure get_transaction(in number_column int, in id int)
+begin 
+	select * from transactions where customer_id = id limit number_column ;
+end $$
+delimiter ;
+
+call get_transaction(2, 2);
+
+
+
+
+
+
+
+
 
 
