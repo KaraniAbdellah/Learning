01733188bcc1e5fbc21ecba4833d26680e158b8e
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 21:45:56 +0100

    create new procedure

--- Learn SQL/learn.sql
+++ Learn SQL/learn.sql
@@ -610,6 +610,32 @@ select * from transactions;
 
 
 
+
+-- STORED PROCEDURES
+/*
+	you can save query if you are using query a lot of time
+*/
+
+-- Example without argument
+delimiter $$
+create procedure get_customers()
+begin 
+	select * from transactions;
+end $$
+delimiter ;
+drop procedure get_customers;
+call get_customers();
+
+-- Example with argument
+delimiter $$
+create procedure get_transaction(in number_column int, in id int)
+begin 
+	select * from transactions where customer_id = id limit number_column ;
+end $$
+delimiter ;
+call get_transaction(2, 2);
+
+
  
 
 
--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -3,12 +3,7 @@
 	you can save query if you are using query a lot of time
 */
 
--- save this as a procedures
--- select * from transactions left join
--- customers on customers.customer_id = transactions.customer_id;
-
 -- Example without argument
-/*
 delimiter $$
 create procedure get_customers()
 begin 
@@ -17,17 +12,14 @@ end $$
 delimiter ;
 drop procedure get_customers;
 call get_customers();
-*/
 
 -- Example with argument
-
 delimiter $$
 create procedure get_transaction(in number_column int, in id int)
 begin 
 	select * from transactions where customer_id = id limit number_column ;
 end $$
 delimiter ;
-
 call get_transaction(2, 2);
 
 
