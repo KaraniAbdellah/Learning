4fc286314a790033e9152c2eecad7d55f5434ef8
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 21:52:20 +0100

    start

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -1,7 +1,15 @@
 -- Triggers
 /*
 	Trigger = When an event happens, do something
+    use trigger: check data, handles errors, auditing tables
+    
+    in this example i am using trigger if
+	update "tran_id" i would update also the "amount"
+		--> "tran_id & amount are columns"
 */
+
+
+select * from transactions;
 
 
 
