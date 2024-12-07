91a73146810fcb5265790608f71d260d4125d579
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 22:54:11 +0100

    start

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,5 +1,14 @@
 // Anonymous Inner Class (Extends Class, Implements Interface, Defined Arguments, Lambda)
+/*
+    Anonymous Inner Class for code more "concise" [brief]
+*/
 
+
+public class App {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
 
 
 
