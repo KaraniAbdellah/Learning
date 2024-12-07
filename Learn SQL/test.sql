1263b384d5736627e305bc843899ba9743ebe6e8
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 21:39:05 +0100

    add rows

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -3,3 +3,23 @@
 	you can save query if you are using query a lot of time
 */
 
+-- save this as a procedures
+-- select * from transactions left join
+-- customers on customers.customer_id = transactions.customer_id;
+
+-- Example without argument
+/*
+delimiter $$
+create procedure get_customers()
+begin 
+	select * from transactions;
+end $$
+delimiter ;
+drop procedure get_customers;
+call get_customers();
+*/
+
+-- Example with argument
+
+
+
