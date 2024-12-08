3a9cd3a5b7cc17bd4ff21b3a547a11f5ebc9ba3b
abdellah karani <abdellahkarani@gmail.com>
2024-12-08 22:43:14 +0100

    add comments

--- Learn SQL/test.sql
+++ Learn SQL/test.sql
@@ -2,25 +2,18 @@
 /*
 	Trigger = When an event happens, do something
     use trigger: check data, handles errors, auditing tables
-    
-    in this example i am using trigger if
-	update "tran_id" i would update also the "amount"
-		--> "tran_id & amount are columns"
 */
 
 -- create trigger that update tran_id
-/*
 create trigger before_tran_id_update
 before update on  transactions -- before (update, delete, insert, ...)
 for each row
 set new.amount = new.tran_id * 2;
 show triggers;
 update transactions set tran_id = 4 where tran_id = 2; -- amount has been change
-*/
 
 
 -- create trigger that update tran_id in insertion new row
-/*
 create trigger update_amount
 before insert on transactions
 for each row
@@ -28,10 +21,9 @@ set new.tran_id = new.amount + 2;
 insert into transactions values(1, 1, 9); -- must see tran_id changed
 select * from transactions;
 drop trigger before_tran_id_update;
-*/
+
 
 -- create table "expenses"
-/*
 create table expenses(
 	expenses_id int primary key auto_increment,
     expenses_name varchar(20),
@@ -42,10 +34,9 @@ insert into expenses values
 	(2, "supplies", 0),
     (3, "taxes", 0)
 ;
-*/
+
 
 -- create trigger that update expenses_total when I delete amount
-/*
 create trigger after_amount_delete 
 after delete on transactions
 for each row 
@@ -56,7 +47,19 @@ where expenses_total = 0;
 delete from transactions where amount = 102; -- expenses_total must change
 select * from expenses;
 select * from transactions;
-*/
+
+
+-- create trigger that if change amount expenses wil be also change
+create trigger update_after_delete
+after update on transactions
+for each row
+update expenses
+set expenses_total = 10 where new.amount != old.amount;
+
+update transactions set amount = 10 where amount = 8; -- all expenses_total = 10 
+select * from transactions;
+select * from expenses;
+
 
 
 
