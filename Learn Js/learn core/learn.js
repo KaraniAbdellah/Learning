c8da06c681c8e12e576bf61bf8d62d6de115fc10
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-12 10:22:39 +0100

    update learn.js

--- Learn Js/learn core/learn.js
+++ Learn Js/learn core/learn.js
@@ -1,3 +1,33 @@
+// NodeJs & npm & require
+/*
+    require:
+        always return a function
+        It loads and uses code from other files or packages.
+
+    Node: 
+        allow you tu run javascript outside
+        the web browser (in the terminal)
+
+    npm:
+        NPM (Node Package Manager), used to install, manage, 
+        and share JavaScript libraries and packages.
+        like github: contain a lot of package
+        npm: contain a lot of packages like "prompt-sync"
+
+        problem: in install any package use -g
+        -g: make ths modulo used in all folders in project
+
+        Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+            change the PowerShell policy to run any script
+        PowerShell: blocks some scripts to protect the computer
+
+    package a lot of modulo and modulo contain a lot of libraries
+
+    "in js you can store function in variable & use variable as function"
+*/
+
+
+
 // Start Learning JavaScript
 /* Method Of Write The Js Code
 Execution => line By Line Must Script Do In last Of Page
