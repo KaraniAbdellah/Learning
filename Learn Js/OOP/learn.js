71d8333e6805aa2a97a62227ac8372a95120dad7
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-16 14:29:30 +0100

    add polymorphisme

--- Learn Js/OOP/learn.js
+++ Learn Js/OOP/learn.js
@@ -222,6 +222,45 @@ console.log(dog.alive); // undefined
 
 
 
+///////////////// Polymorphism
+/*
+    We realize polymorphism with
+        Overriding --> paractice this in inhertiance
+        Overloading --> you can use default paramerters
+
+    JavaScript does not natively support method overloading
+*/
+
+// Overinding
+class Animal {
+    speak() {
+        console.log("This animal makes a sound.");
+    }
+}
+class Dog extends Animal {
+    // @Override
+    speak() {
+        console.log("The dog barks.");
+    }
+}
+const animal = new Animal();
+animal.speak(); // This animal makes a sound.
+ 
+// Overloading [use default parameters]
+class Calculator {
+    add(a, b, c = 0) {
+      return a + b + c;
+    }
+}
+const calc = new Calculator();
+console.log(calc.add(2, 3));
+console.log(calc.add(2, 3, 4));
+
+
+
+
+
+
 
 
 
--- Learn Js/OOP/test.js
+++ Learn Js/OOP/test.js
@@ -1,69 +1,36 @@
-// Inheritance & super keyword 
+// Polymorphism
 /*
-    - Introduction
-        allows a new class to use the attributes
-        and methods of another class
-    
-    Use protected signe (#) in classes when there is inheritance
-    Classes can only extend a single class.
-    Static methods are not inherited from a parent class to a child class.
+    We realize polymorphism with
+        Overriding --> paractice this in inhertiance
+        Overloading --> you can use default paramerters
+
+    JavaScript does not natively support method overloading
 */
 
+// Overinding
 class Animal {
-    #name
-    #age
-    static alive = true; // shared between all objets
-
-    constructor(name, age) {
-        this.#name = name;
-        this.#age = age;
-    }
-    eat() {
-        console.log("Eat " + this.#name);
-    }
-    sleep() {
-        console.log("Sleep " + this.#name);
-    }
-    getName() {
-        return this.#name;
+    speak() {
+        console.log("This animal makes a sound.");
     }
 }
-class Fich extends Animal {
-    #isSwim
-    constructor(name, age, isSwim) {
-        super(name, age);
-        this.#isSwim = isSwim;
-    }
-    swim() {
-        if (this.#isSwim) console.log(this.getName() + " is Swim");
-        else console.log(this.getName() + " is not Swim");
+class Dog extends Animal {
+    // @Override
+    speak() {
+        console.log("The dog barks.");
     }
 }
-class Dog extends Fich {
-    #is_nice
-    constructor(name, age, isSwim, is_nice) {
-        super(name, age, isSwim);
-        this.#is_nice = is_nice;
-    }
-    isNice() {
-        if(this.#is_nice) console.log(this.getName() + " is nice dog");
-        else console.log(this.getName() + " is not nice dog");
+const animal = new Animal();
+animal.speak(); // This animal makes a sound.
+ 
+// Overloading [use default parameters]
+class Calculator {
+    add(a, b, c = 0) {
+      return a + b + c;
     }
 }
+const calc = new Calculator();
+console.log(calc.add(2, 3));
+console.log(calc.add(2, 3, 4));
 
-const animal1 =  new Animal("Animal", 20);
-console.log(Animal.alive); // true
-animal1.eat(); // Eat Animal
-
-const fich = new Fich("Sheren", 2, true);
-fich.eat(); // Eat Sehren
-fich.swim(); // Shreni Swim
-
-const dog = new Dog("putbool", 12, false);
-dog.eat(); // Eat putbool
-dog.swim(); // putbool is not Swim
-dog.isNice(); // putbool is not nice dog
-
-console.log(dog.alive); // undefined
 
 
