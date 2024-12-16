b9ea5e6b310799e1c0ae4aacda5f5092702ff987
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-16 15:40:17 +0100

    start new topic of solid principles

--- Learn Js/OOP/test.js
+++ Learn Js/OOP/test.js
@@ -1,31 +1,5 @@
-// Abstraction || Interface
-/*
-    Js does not support interfaces
-    The two concept support in Typescript
-    
-    We can achieve interfaces:
-        create a function that check if object implement method
-*/
-
-// this function check if object implements a specific methods
-function isImplementMethod(obj) {
-    if(obj.display == undefined) {
-        throw new Error("Class must implement 'display' method");
-    }
-}
-// Implement the interface in a class
-class Product {
-    constructor() {
-        // isImplementMethod.call(this); // pass object without parameter
-        isImplementMethod(this); 
-    }
-    display() {
-        console.log("display the details here");
-    }
-}
-
-const product = new Product();
-product.display(); // display the details here
-
-
+// Classes et méthodes génériques
+// Garbage Collection avec gestion de mémoire JavaScript)
+// - Principes SOLID (Single Responsibility, Open-Closed, Liskov Substitution, Interface Segregation, Dependency Inversion)
+// - Conception de classes et relations entre objets avec Node.js
 
