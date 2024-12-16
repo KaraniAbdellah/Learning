848dccce2b6bfad6a0cbc4b85928d3cca1b074d3
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-16 11:36:01 +0100

    add images & finich the encapsulation

--- Learn Js/OOP/learn.js
+++ Learn Js/OOP/learn.js
@@ -1,8 +1,14 @@
 // Start Learn OOP
 /*
-    
+    - Classes and objets
+    - Encapsulation
+    - Inheritance
+    - Abstraction (abstract class, interface)
+    - Garbage Collocter
 */
-// Classes and objets
+
+
+/////////////////// Classes and objets
 /*
     - Classes are a template for creating objects
     - Classes is ES6 features
@@ -45,6 +51,96 @@ const employee = new Employee("ahmed", 20202);
 employee.DisplayEmployee();
 Employee.Bonus();
 // employee.Bonus(); employee.Bonus is not a function
+
+
+
+
+
+///////////////// Encapsulation
+/*
+    make the attribute private & methods public
+    use setters & getters functions
+    you can use set & get keyword
+
+    +: public 
+    #: protected --> use in inheritance
+    _: private
+*/
+
+class Rectangle {
+    #width // protected
+    _height // private
+    _value
+    
+    constructor(width, height, value, count) {
+        this.#width = width;
+        this._height = height;
+        this._value = value;
+        this.count = count; // public
+    }
+    // Method 1: setters & getters
+    setWidth(width) {
+        this.#width = width;
+    }
+    getWidth() {
+        return this.#width;
+    }
+    setHeight(height) {
+        this._height = height;
+    }
+    getHeight() {
+        return this._height; 
+    }
+    setValue(height) {
+        this._height = height;
+    }
+    getValue() {
+        return this._value; 
+    }
+    setValue(value) {
+        this._value = value; 
+    }
+    // Declare Private Method
+    _DisplayRectangle() {
+        return "display";
+    } 
+    // Use Private Method
+    display() {
+        return this._DisplayRectangle();
+    }
+    // Method 2
+    set height(newWidth) {
+        this._height = newWidth;
+    }
+    get height() {
+        return this._height;
+    }
+}
+
+const rect = new Rectangle(1222, 1929, 100, 1010);
+
+// Method 1
+console.log("count = " + rect.count); // count is public attribute
+// console.log(rect.DisplayRectangle()); private method
+console.log(rect.display()); // "display"
+console.log(rect.getHeight()); // 1929
+console.log(rect.getValue()); // 100$
+
+// Method 2
+rect.height = 1001;
+console.log("newHeight = " + rect.getHeight());
+console.log("newHeight = " + rect.height);
+
+
+
+
+
+///////////////// Inheritance
+
+
+
+
+
 
 
 
--- Learn Js/OOP/test.js
+++ Learn Js/OOP/test.js
@@ -1,24 +1,25 @@
 // Encapsulation
 /*
     make the attribute private & methods public
-    use setters & getters
+    use setters & getters functions
+    you can use set & get keyword
 
-    ES6+ Private Fields:
-        use #
-    Getters and Setters
-        use _
+    +: public 
+    #: protected --> use in inheritance
+    _: private
 */
 
 
 class Rectangle {
-    #width
-    #height
+    #width // protected
+    _height // private
     _value
     
-    constructor(width, height, value) {
+    constructor(width, height, value, count) {
         this.#width = width;
-        this.#height = height;
+        this._height = height;
         this._value = value;
+        this.count = count; // public
     }
     // Method 1: setters & getters
     setWidth(width) {
@@ -28,10 +29,10 @@ class Rectangle {
         return this.#width;
     }
     setHeight(height) {
-        this.#height = height;
+        this._height = height;
     }
     getHeight() {
-        return this.#height; 
+        return this._height; 
     }
     setValue(height) {
         this._height = height;
@@ -39,12 +40,37 @@ class Rectangle {
     getValue() {
         return this._value; 
     }
+    setValue(value) {
+        this._value = value; 
+    }
     // Declare Private Method
-    #DisplayRectangle() {
-        console.log();
+    _DisplayRectangle() {
+        return "display";
     } 
+    // Use Private Method
+    display() {
+        return this._DisplayRectangle();
+    }
+    // Method 2
+    set height(newWidth) {
+        this._height = newWidth;
+    }
+    get height() {
+        return this._height;
+    }
 }
-const rect = new Rectangle(1222, 1929, 100);
+
+const rect = new Rectangle(1222, 1929, 100, 1010);
+
+// Method 1
+console.log("count = " + rect.count); // count is public attribute
+// console.log(rect.DisplayRectangle()); private method
+console.log(rect.display()); // "display"
 console.log(rect.getHeight()); // 1929
-console.log(rect.getValue()); // 100
+console.log(rect.getValue()); // 100$
+
+// Method 2
+rect.height = 1001;
+console.log("newHeight = " + rect.getHeight());
+console.log("newHeight = " + rect.height);
 
