ad215115ed819ccf3c5c055a16693e80216b234b
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-20 11:22:11 +0100

    add images

---
+++ Learn Js/solid_principles/learn.js
@@ -0,0 +1,22 @@
+// Solid Principles
+/*
+    1. Single Responsibility Principle
+    2. Open/Closed Principle
+    3. Liskov Substitution Principle
+    4. Interface Segregation Principle
+    5. Dependency Inversion Principle
+*/
+
+
+// Single Responsibility Principle
+/*
+    A class should have one job, and one reasons to change.
+*/
+
+
+
+
+
+
+
+
