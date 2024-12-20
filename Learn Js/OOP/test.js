e4d64ada4b2e0cf0c78c09c1a3875048dfe8cc65
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-20 10:57:48 +0100

    remove file

--- Learn Js/OOP/test.js
+++ Learn Js/OOP/test.js
@@ -1,13 +1,3 @@
-// Solid Principles
-/*
-    - Single Responsibility
-    - Open-Closed
-    - Liskov Substitution
-    - Interface Segregation
-    - Dependency Inversion
-*/
 
-// https://medium.com/swlh/solid-principles-of-oop-c24bd6ccde77
-// https://www.youtube.com/playlist?list=PLwWuxCLlF_uevri_OpofVLXkRRFnZ7TSV
-
+// Look the Other file about solid principles
 
