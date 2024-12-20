d1d9a2b70ce29e1237afc371c71403aac4bf4c4a
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-20 11:54:38 +0100

    add SRP

--- Learn Js/solid_principles/learn.js
+++ Learn Js/solid_principles/learn.js
@@ -5,16 +5,50 @@
     3. Liskov Substitution Principle
     4. Interface Segregation Principle
     5. Dependency Inversion Principle
+
 */
 
 
 // Single Responsibility Principle
 /*
-    A class should have one job, and one reasons to change.
+    - A class should have one job --> one reasons to change.
+    - Benifit:
+        easy to understand
+        organized
+        reduces dependencies by ensuring each class has one purpose
+        if you create new class --> link the classes by composition
 */
 
+class Customer {
+    _name
+    constructor (name) {
+        this._name = name;
+    }
+    getName() {
+        return this._name;
+    }
+    // This is not good --> create new class
+    storeCutomers() {
+        // store customer in database
+    }
+    // This is not good --> create new class
+    generateCustomerReport() {
+        // Generate a report...
+    }
+}
 
+class CustomerDb {
+    customer = new Customer(); // composition
+    storeCutomers() {
+        // store customer in database
+    }
+}
 
+class CustomerReport {
+    generateCustomerReport() {
+        // Generate a report...
+    }
+}
 
 
 
