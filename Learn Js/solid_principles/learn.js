10efa9b654771eacce2849d2e4c164c66bb4da9e
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-25 17:38:57 +0100

    start  Liskov substitution

--- Learn Js/solid_principles/learn.js
+++ Learn Js/solid_principles/learn.js
@@ -37,20 +37,82 @@ class Customer {
     }
 }
 
-class CustomerDb {
+class CustomerDb extends Customer{
     customer = new Customer(); // composition
     storeCutomers() {
         // store customer in database
     }
 }
 
-class CustomerReport {
+class CustomerReport extends Customer {
     generateCustomerReport() {
         // Generate a report...
     }
 }
 
+export default Customer; // this class can not be change
 
+
+
+
+// Open-Closed
+
+/*
+    - software(classes, modules, functions, etc).
+        should be open for extention, but closed for modification
+
+    - we need abstraction and polymorphisme
+*/
+
+
+class Rectangle {
+    _width
+    _height
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    } 
+}
+class Square {
+    _side
+    constructor(side) {
+        this._side = side;
+    }
+}
+class ShapePrinter {
+    drawShape(shape) {
+        if (shape instanceof Square) {
+            // Draw Square ...
+        } else if (shape instanceof Rectangle) {
+            // Draw Rectangle ...
+        } 
+        // the problem when i create another form (Box) --> must add new condition
+    }
+}
+
+// Solution Create Abstract Class
+class Shape {
+    draw() {
+        // Draw ...
+    }
+}
+class Rectangle extends Shape {
+    // Override 
+    draw() {
+        // Draw Rectangle
+    }
+}
+class Square extends Shape {
+    // Override 
+    draw() {
+        // Draw Square
+    }
+}
+class SahpePrinter {
+    draw(shape) { // shape if type shape
+        shape.draw(); // any one can draw
+    }
+}
 
 
 
