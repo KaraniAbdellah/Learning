858f665e17505b634568f51f49f92c79439c7cbd
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-28 22:53:52 +0100

    changes

--- Learn Java/App.java
+++ Learn Java/App.java
@@ -1,15 +1,11 @@
-// Anonymous Inner Class (Extends Class, Implements Interface, Defined Arguments, Lambda)
-/*
-    Anonymous Inner Class for code more "concise" [brief]
-*/
 
-import Anonymous_Inner_Class.*;
+
 
 public class App {
     public static void main(String args[]) {
-        Reward reward = new Reward();
-        reward.rewardMethod();
-    }
+        System.out.println("Hello World");
+    } 
 }
+
 
 
