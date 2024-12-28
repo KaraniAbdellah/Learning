f97c404c8caf05dde1bbd89be5fb88a725f8021f
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-28 22:44:53 +0100

    add tw classes

--- Learn Js/solid_principles/test.js
+++ Learn Js/solid_principles/test.js
@@ -1,11 +1,8 @@
 // Liskov substitution
-
-
 /*
     if Class S subtype of T(parent). then objects of type T
     in a program my be replaced with Objects S.
 */
-
 
 class Rectangle {
     _width
@@ -20,7 +17,6 @@ class Rectangle {
         this._height = newHeight;
     }
 }
-
 class Square extends Rectangle {
     // @Override
     setWidth(newWidth) {
@@ -33,12 +29,10 @@ class Square extends Rectangle {
         super.setWidth(newHeight);
     }
 }
-
 let rectangle = new Rectangle();
 rectangle.setHeight(2);
 rectangle.setWidth(5);
 if (rectangle.getArea() == 10) console.log("is 10");
-
 
 // the problem here ---> square must have also 10
 let square = new Square();
@@ -48,11 +42,11 @@ if (square.getArea() == 10) console.log("is 10");
 else console.log("is not 10");
 
 
+
 // Solution -->> Shape is interface
 class Shape {
     area() {}
 }
-
 // Rectangle impelment Shape
 class Rectangle1 extends Shape {
     _width
@@ -73,7 +67,6 @@ class Square1 extends Shape {
     }
     setSize(newSize) { this._size = newSize; }
 }
-
 let rectangle1 = new Rectangle1();
 rectangle1.setHeight(2); rectangle1.setWidth(5);
 let square1 = new Square1();
@@ -81,6 +74,29 @@ square1.setSize(2);
 
 if (rectangle1.area() == 10) console.log("is 10");
 if (square1.area() == 4) console.log("is 4");
+
+
+
+// Another Solution
+/* Rectangle extend from Saqure but in Squqre --> create constructor */
+class Rectangle2 {
+    _width
+    _height
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+    getArea() {
+        return this._width * this._height;
+    }
+}
+class Square2 extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    }
+}
+let rectangle2 = new Rectangle2(2, 5);
+let square2 = new Square2(2);
 
 
 
