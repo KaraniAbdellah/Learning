7e23da6363f2933595d2a3213d37c0c21acc28ae
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-30 21:49:14 +0100

    start Dependency inversion

---
+++ Learn Js/solid_principles/Dependency_inversion.ts
@@ -0,0 +1,16 @@
+// Dependency inversion
+/*
+    - high level module should not depend in low level module
+        Both should depend in abstraction
+    - Abstraction Should not depend in details
+*/
+
+
+
+
+
+
+
+
+
+
