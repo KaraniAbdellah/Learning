c5dc7f31e4f623916d1209826ae70d9a98edf1bc
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-30 22:10:37 +0100

    add classes

--- Learn Js/solid_principles/Dependency_inversion.ts
+++ Learn Js/solid_principles/Dependency_inversion.ts
@@ -2,15 +2,46 @@
 /*
     - high level module should not depend in low level module
         Both should depend in abstraction
-    - Abstraction Should not depend in details
+    - Abstraction Should not depend in details (concrete implementation)
+        should depend on abstractions
 */
 
 
+class DeliverDriver {
+    deliverProduct(): void {
+        // delevier product ...
+    }
+}
 
+class DeliveryCompany {
+    SendProduct(): boolean {
+        const d: DeliverDriver = new DeliverDriver();
+        d.deliverProduct();
+        return true;
+    }
+}
 
+// Solution
+interface DeliverService {
+    deliverProduct(): void;
+}
 
+class DeliverDriver1 implements DeliverService {
+    deliverProduct(): void { }
+}
 
+class DeliveryCompany1 implements DeliverService {
+    deliverProduct(): void {
 
+    }
+    private deliver_service: DeliverService;
+    constructor(deliver_service: DeliverService) {
+        this.deliver_service = deliver_service;
+    }
+    sendProduct() {
+        this.deliver_service.deliverProduct();
+    }
+}
 
 
 
