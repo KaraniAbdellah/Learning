95d63e4272acf715406597d4dfd311ed2e90bf62
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-06 17:32:36 +0100

    start dom manipulation

--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,35 +1,4 @@
-// Effects
-/*
-    animate()
-    stop()
-    callback
-    chaining
-*/
-
-$(document).ready(function () {
-    $(".login").click(function () {
-        $(".square").animate({
-            top: "500px"
-        }, 2000, "linear"); // animate the element
-    });
-
-    $(".stop").click(function() {
-        $(".square").stop(); // stop the event
-    });
-
-    // Call back
-    $(".hide").click(function() {
-        $(".square").hide(1000, function() {
-            console.log("Hide Element Succefully"); // after hidding ele
-        });
-    });
-
-    // Chaining
-    $(".square").hide(1000).show(1000).slideUp();
-});
-
-
-
+// DOM Manipulation
 
 
 
