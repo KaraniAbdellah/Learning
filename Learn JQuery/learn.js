9e04a000454b6dc3afecc3989b45a1f1f38f55fd
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-06 17:31:52 +0100

    add effects

--- Learn JQuery/learn.js
+++ Learn JQuery/learn.js
@@ -59,3 +59,37 @@ $(document).ready(function () {
 });
 
 
+
+
+
+
+// Effects
+/*
+    animate()
+    stop()
+    callback
+    chaining
+*/
+
+$(document).ready(function () {
+    $(".login").click(function () {
+        $(".square").animate({
+            top: "500px"
+        }, 2000, "linear"); // animate the element
+    });
+
+    $(".stop").click(function() {
+        $(".square").stop(); // stop the event
+    });
+
+    // Call back
+    $(".hide").click(function() {
+        $(".square").hide(1000, function() {
+            console.log("Hide Element Succefully"); // after hidding ele
+        });
+    });
+
+    // Chaining
+    $(".square").hide(1000).show(1000).slideUp();
+});
+
--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,25 +1,36 @@
-// Events
+// Effects
+/*
+    animate()
+    stop()
+    callback
+    chaining
+*/
+
 $(document).ready(function () {
-    $(".login").click(function() {
-        alert("Hello World");
-        $("#demo").hide(1000); // hide the ele in 1 second 
-        $("#demo").show(); // show the ele
-        $(".ele").toggle(); // can be hide or show
-
-        $(".square").fadeIn(); // display the ele with animation
-        $(".square").fadeOut(2000); // hide the ele with animation
-        $(".square").fadeToggle(); // display or show the ele
-
-        $("square").slideDown(); // display the ele liek slide animation
-        $("square").slideUp();
-        $("square").slideToggle(); // display ele in down and top 
+    $(".login").click(function () {
+        $(".square").animate({
+            top: "500px"
+        }, 2000, "linear"); // animate the element
     });
+
+    $(".stop").click(function() {
+        $(".square").stop(); // stop the event
+    });
+
+    // Call back
+    $(".hide").click(function() {
+        $(".square").hide(1000, function() {
+            console.log("Hide Element Succefully"); // after hidding ele
+        });
+    });
+
+    // Chaining
+    $(".square").hide(1000).show(1000).slideUp();
 });
 
 
 
-// $(".square").animate({
-//     top: "100px"
-// });
+
+
 
 
