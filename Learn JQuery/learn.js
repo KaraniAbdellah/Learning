67106281c5e5a97fb47402857391d956a460f612
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-06 16:40:07 +0100

    add selectos and syntax

--- Learn JQuery/learn.js
+++ Learn JQuery/learn.js
@@ -2,9 +2,38 @@
 /*
     is js libarary [write less, do more]
     Donwload JQuery
-    Include Jquery in HTML file ==> the jquery file must be before js files
-     
+    Include Jquery in HTML file ==> the jquery file must be before js files 
 */
+
+
+
+
+// Syntax 
+/*
+    we use always ==> $ == JQuery
+    document is html page
+    ready mean when html page ready
+*/
+$(document).ready(function() {
+    // Every thing here
+}); // do not write outside
+// Another syntax
+$(function() {
+    // Every thing here
+});
+ 
+
+
+
+// Selectors
+$(document).ready(function() {
+    // select ele
+    $("section")
+    // select class
+    $(".ele")
+    // select id
+    $("#id")
+});
 
 
 
--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,3 +1,29 @@
 // Syntax 
+/*
+    we use always ==> $ == JQuery
+    document is html page
+    ready mean when html page ready
+*/
+$(document).ready(function() {
+    // Every thing here
+}); // do not write outside
+// Another syntax
+$(function() {
+    // Every thing here
+});
+ 
+
+
+// Selectors
+$(document).ready(function() {
+    // select ele
+    $("section")
+    // select class
+    $(".ele")
+    // select id
+    $("#id")
+});
+
+
 
 
