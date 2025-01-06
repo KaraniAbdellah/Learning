e46d70e24a2ce154ce6ee1b7dbb9283c7b0ea092
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-06 17:56:35 +0100

    add DOM manpulation

--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,5 +1,24 @@
 // DOM Manipulation
 
 
+$(document).ready(function() {
+    let myText =  $(".sayHello").text(); // get text
+    $(".sayHello").text("this new text"); // set text
+    console.log(myText);
+
+    let htmlText = $(".sayHello").html(); // get html tag 
+    $(".sayHello").html("this new text"); // set html tag
+    console.log(htmlText); // <a href="">sayHello</a>
+
+    let inputValue = $("input").val();
+    $("input").val("write your name");
+    console.log(inputValue); // write your email
+
+    let valId = $("div").attr("id"); // get the value of "id" attribut
+    $(".card").attr("class", "product"); // set new attribut (class = "product")  [override to old classes]
+    console.log(valId);
+
+}); // do not write code outside
+
 
 
