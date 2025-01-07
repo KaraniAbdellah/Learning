6174d3456fcfdf2cd584a7eab2c91a98bdce7b44
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-07 08:38:10 +0100

    add traversing

--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,4 +1,30 @@
 // Traversing
+/*
+    Ancestors [parents, parent, parentsUntil]
+    Descendants[children, find]
+    Sibling[siblings, next, nextAll, prev, prev]
+*/
+$(function() {
+    // Ancestors
+    $(".span").parent().css("color", "red");
+    $(".span").parents().css("color", "red");
+    console.log($(".span").parents()); // body & html is also parents
+
+    $(".span").parents("ul"); // give me just the ul parent
+    console.log($(".span").parents("ul"));
+
+    $(".span").parentsUntil("section"); // give me all parents until section
+    console.log( $(".span").parentsUntil("section")); // section is not included
+
+    // Descendants
+    $(".section").children(); // get the direct ele 
+    console.log($(".section").children()); // [div]
+
+    $(".section").find("ul").css('color', "blue");
+    console.log($(".section").find("ul"));
+
+
+});
 
 
 
