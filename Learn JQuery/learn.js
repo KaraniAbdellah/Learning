e26eecfa2a5520f8495626d3a650a3e508ea1028
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-07 09:57:17 +0100

    add travsing

--- Learn JQuery/learn.js
+++ Learn JQuery/learn.js
@@ -163,3 +163,53 @@ $(function() {
 
 
 
+
+
+
+// Traversing
+/*
+    Ancestors [parents, parent, parentsUntil]
+    Descendants[children, find]
+    Sibling[siblings, next, nextAll, prev, prevAll, nextUntil, prevUntill]
+*/
+$(function() {
+    // Ancestors
+    $(".span").parent().css("color", "red");
+    $(".span").parents().css("color", "red");
+    console.log($(".span").parents()); // body & html is also parents
+
+    $(".span").parents("ul"); // give me just the ul parent
+    console.log($(".span").parents("ul"));
+
+    $(".span").parentsUntil("section"); // give me all parents until section
+    console.log( $(".span").parentsUntil("section")); // section is not included
+    console.log("==============================");
+
+    // Descendants
+    $(".section").children(); // get the direct ele 
+    console.log($(".section").children()); // [div]
+
+    $(".section").find("ul").css('color', "blue");
+    console.log($(".section").find("ul"));
+    console.log("==============================");
+
+
+    // Sibling
+    $(".desc").siblings();
+    console.log($(".desc").siblings());
+    console.log($(".desc").siblings("span")); // get the span
+
+    console.log($(".desc").nextAll()); // div, span, ul
+    console.log($(".desc").next()); // div
+
+    console.log($(".list").prev()); // span
+    console.log($(".list").prevAll()); // span, div, p.desc
+
+
+    console.log($(".list").prevUntil("p.desc")); // span, div
+    console.log($(".desc").nextUntil(".list")); // div, span
+
+});
+
+
+
--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -2,7 +2,7 @@
 /*
     Ancestors [parents, parent, parentsUntil]
     Descendants[children, find]
-    Sibling[siblings, next, nextAll, prev, prev]
+    Sibling[siblings, next, nextAll, prev, prevAll, nextUntil, prevUntill]
 */
 $(function() {
     // Ancestors
@@ -15,6 +15,7 @@ $(function() {
 
     $(".span").parentsUntil("section"); // give me all parents until section
     console.log( $(".span").parentsUntil("section")); // section is not included
+    console.log("==============================");
 
     // Descendants
     $(".section").children(); // get the direct ele 
@@ -22,9 +23,23 @@ $(function() {
 
     $(".section").find("ul").css('color', "blue");
     console.log($(".section").find("ul"));
+    console.log("==============================");
+
 
     // Sibling
-    
+    $(".desc").siblings();
+    console.log($(".desc").siblings());
+    console.log($(".desc").siblings("span")); // get the span
+
+    console.log($(".desc").nextAll()); // div, span, ul
+    console.log($(".desc").next()); // div
+
+    console.log($(".list").prev()); // span
+    console.log($(".list").prevAll()); // span, div, p.desc
+
+
+    console.log($(".list").prevUntil("p.desc")); // span, div
+    console.log($(".desc").nextUntil(".list")); // div, span
 
 });
 
