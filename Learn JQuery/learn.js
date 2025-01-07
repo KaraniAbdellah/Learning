fc772a74bbca278884b3cbfb89c67239f8ebe2d5
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-07 10:14:18 +0100

    start Aja

--- Learn JQuery/learn.js
+++ Learn JQuery/learn.js
@@ -213,3 +213,22 @@ $(function() {
 
 
 
+
+
+// Traversing
+/*
+    Filter[first, last, eq, filter, not]
+*/
+$(function() {
+
+    $("p.desc").first();
+    console.log($("p.desc").first()); // first paragph that has .desc class
+    console.log($("p.desc").last()); // last paragph that has .desc class
+    console.log($("p.desc").eq()); // middle paragph that has .desc class
+    console.log($("p.desc").eq(2)); // paragph number 2  that has .desc class
+
+    $(".test1").filter(2);
+    console.log($("p").filter(".test1")); // get all paragraph that has class .test1
+    console.log($("p").not(".test1")); // get all paragraph that has not class .test1
+
+});
