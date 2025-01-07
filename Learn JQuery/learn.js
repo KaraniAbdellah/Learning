02dc3ed851b47450fd080e462738dadd602eaa50
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-07 08:09:05 +0100

    star Dimension

--- Learn JQuery/learn.js
+++ Learn JQuery/learn.js
@@ -119,3 +119,32 @@ $(document).ready(function() {
 
 
 
+
+
+
+// DOM Manipulation [Adding & Removing]
+/*
+    add & remove [append, prepend, after, before, remove]
+    css [addClass, removeClass, toggleClass]
+*/
+$(function() {
+    $("p.title").append(" Append");
+    $("p.title").prepend("Prepend ");
+    
+    $("p.title").after("After");
+    $("p.title").before("Before");
+
+    $("p.title").remove(); // remove tag
+    
+    $("p.title").addClass("main ");
+    $("p.title").removeClass("main ");
+    $("p.title").toggleClass("main ");
+    $("p.title").css({
+        fontSize: "40px",
+    });
+    $("p.title").css('color', 'red');
+});
+
+
+
+
--- Learn JQuery/test.js
+++ Learn JQuery/test.js
@@ -1,25 +1,4 @@
-// DOM Manipulation
-/*
-    add & remove [append, prepend, after, before, remove]
-    css [addClass, removeClass, toggleClass]
-*/
-$(function() {
-    $("p.title").append(" Append");
-    $("p.title").prepend("Prepend ");
-    
-    $("p.title").after("After");
-    $("p.title").before("Before");
-
-    // $("p.title").remove(); // remove tag
-    
-    $("p.title").addClass("main ");
-    $("p.title").removeClass("main ");
-    $("p.title").toggleClass("main ");
-    $("p.title").css({
-        fontSize: "40px",
-    });
-    $("p.title").css('color', 'red');
-});
+// DOM Manipulation []
 
 
 
