c5090594cb9287c36b74b9e16331fcd55f88d3f4
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-28 17:05:34 +0100

    react awesome

---
+++ Learn react/Learning/learn.js
@@ -0,0 +1,40 @@
+// What is React Js
+/*
+    A free and open-source JavaScript library
+    :::::::::::::::::::
+    developed by Meta (formerly Facebook).
+    :::::::::::::::::::
+    React Based on the concept of Component
+    :::::::::::::::::::
+    Some Concepts:
+        Vite: A fast tool to start React projects.
+        JSX: A way to write HTML inside JavaScript.
+        Components: Small parts of a webpage you can reuse.
+        Props: Data you give to components.
+        State: Data that changes inside a component.
+        Hooks: Special functions, like useState to manage state.
+        Virtual DOM: React's way to update only what changes on the page.
+    :::::::::::::::::::
+    From Component (Button) --> <button>Click Here</button>
+        The Switch From Component To tags do by React Not Browser
+    :::::::::::::::::::
+    Write React code directly without Framworks like Next.js or Remix.
+    Use only React and maybe libraries like Vite or Webpack to build your app.
+    Handle everything manually, like routing or server tasks.
+    :::::::::::::::::::
+    Vite: Fast tool for React.
+    Next.js: Full framework with more features (Routing, SSR, APIs).
+*/
+
+
+// Installation
+/*
+    npx create-react-app learn
+*/
+
+
+
+
+
+
+
