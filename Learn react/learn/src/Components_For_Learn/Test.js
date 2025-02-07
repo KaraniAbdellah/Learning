3d0cb5234778299972e32f271d0138d96f5c00d4
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-07 19:03:01 +0100

    update arrays in state

--- Learn react/learn/src/Components_For_Learn/Test.js
+++ Learn react/learn/src/Components_For_Learn/Test.js
@@ -1,33 +1,30 @@
 // Start Create Test Component
 import "./Test.css"
 
-// Update Objects in state
-/*
-	use ...PrevCar and Updater Function and ()
-		in arrow function if I use {} ==> javascript except multilple lines
-			ex: (prevCar) => {...prevCar, make: e.target.value}
-*/
+// Update Ararays in State 
+
 
 import React, {useState} from "react";
 function Test(props) {
-	const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
-
-	const HandleMakeChange = (e) => {
-		setCar(prevCar => ({...prevCar, make: e.target.value}));
+	const [foods, setFoods] = useState(["Apple", "Banana", "Orange"]);
+	async function handleAddEle() {
+		const newFood = document.getElementById("foodInput");
+		await setFoods((foods) => [...foods, newFood.value]);
+		newFood.value = "";
 	}
-	const HandleModelChange = (e) => {
-		setCar(prevCar => ({...prevCar, model: e.target.value}));
+	const handleRemoveEle = (index) => {
+		setFoods(foods.filter((_, i) => i !== index));
 	}
-	const HandleYearChange = (e) => {
-		setCar(prevCar => ({...prevCar, year: e.target.value}));
-	}
-
 	return (
 		<div>
-			<p>Your favorite Car is: {car.make} {car.model} @{car.year}</p>
-			<input onChange={(e) => HandleMakeChange(e)} defaultValue={car.make} type="text"/>
-			<input onChange={(e) => HandleModelChange(e)} type="text" defaultValue={car.model}/>
-			<input onChange={(e) => HandleYearChange(e)} type="text" defaultValue={car.year}/>
+			<h1>List Of Foods: </h1>
+			<ul>
+				{foods.map((food, index) => <li key={index} onClick={() => handleRemoveEle(index)}>{food}</li>)}
+			</ul>
+			<div className="submit">
+				<input type="text" id="foodInput" placeholder="Enter your favorite food"/>
+				<button onClick={handleAddEle}>Add Food</button>
+			</div>
 		</div>
 	);
 }
