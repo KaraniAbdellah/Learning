1802e0731e9f89de6229a5e3d174b58628b5d98f
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-11 15:46:46 +0100

    add useRef

--- Learn react/Learning/learn.js
+++ Learn react/Learning/learn.js
@@ -812,6 +812,46 @@ function CompC() {
 
 
 
+// useRef()
+/*
+	useRef(): "use Refrence" Does not cause re-renders when it values changes.
+		When you want a component to "remember" some information,
+		but you don't want that that information to trigger new renders.
 
-// 
+		1. Accessing/Interacting/ with DOM elements
+		2. Handling Focus, Animations and Transitions
+		3. Managing TImers, and Itervals
+	
+	// Without Ref:
+		const [number, setNumber] = useState(0);
+		const handleClick = () => {
+			console.log(number);
+			setNumber(n => n + 1);
+		}
+		<button onClick={() => handleClick()}>Click Me!</button>
+	
+	NOTE: I can link ref with value and also with element
+		<input ref={InputRef} type="text"/>
+*/
+
+import { useRef, useEffect } from "react";
+function Test() {
+	const inputRef = useRef(null);
+	useEffect(() => {
+	console.log("COMPONENT RENDERED");
+	});
+
+	const handleClick = () => {
+	if (inputRef.current) {
+		inputRef.current.style.backgroundColor = "yellow";
+	}
+	};
+
+	return (
+	<div>
+		<input ref={inputRef} type="text" />
+		<button onClick={handleClick}>Click Me!</button>
+	</div>
+	);
+}
 
--- Learn react/learn/src/Components_For_Learn/Test.js
+++ Learn react/learn/src/Components_For_Learn/Test.js
@@ -1,38 +1,56 @@
+
 // Start Create Test Component
 import "./Test.css"
 
 
-// useContxt()
+// useRef()
 /*
-	React Hook that alow you share values between multiple levels
-		of components without passing props through each level.
-	
-		
-	// PROVIDER COMPONENTS
-		import {createContext} from "react";
-		export const MyContext = createContext()
-		<MyContext.Provider value = {value}>
-			<Child/>
-		<MyContext.Provider>
+	useRef(): "use Refrence" Does not cause re-renders when it values changes.
+		When you want a component to "remember" some information,
+		but you don't want that that information to trigger new renders.
 
-	// CONSUMER COMPONENTS
-		import React, {useContext} from "react";
-		import { MyContext } from "./ComponentA";
-		const value = useContext(MyContext);
+		1. Accessing/Interacting/ with DOM elements
+		2. Handling Focus, Animations and Transitions
+		3. Managing TImers, and Itervals
+	
+	
+	// Without Ref:
+		const [number, setNumber] = useState(0);
+		const handleClick = () => {
+			console.log(number);
+			setNumber(n => n + 1);
+		}
+		<button onClick={() => handleClick()}>Click Me!</button>
+	
+	NOTE: I can link ref with value and also with element
+		<input ref={InputRef} type="text"/>
 */
 
-import CompA from "./CompA";
 import "./style.css";
 
-import React, {useState, useEffect, useContext} from "react";
-function Test(props) {
-	const [user, setUser] = useState();
+import { useRef, useEffect } from "react";
+
+function Test() {
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+	console.log("COMPONENT RENDERED");
+	});
+
+	const handleClick = () => {
+	if (inputRef.current) {
+		inputRef.current.style.backgroundColor = "yellow";
+	}
+	};
+
 	return (
-		<div>
-			<CompA></CompA>
-		</div>
+	<div>
+		<input ref={inputRef} type="text" />
+		<button onClick={handleClick}>Click Me!</button>
+	</div>
 	);
 }
+
 
 export default Test;
 
