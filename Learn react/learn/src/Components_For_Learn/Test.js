950454929bde018cb13ab137712b7ecfe5715eff
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-12 09:37:41 +0100

    add content

--- Learn react/learn/src/Components_For_Learn/Test.js
+++ Learn react/learn/src/Components_For_Learn/Test.js
@@ -1,58 +1,14 @@
-
 // Start Create Test Component
-import "./Test.css"
+import React, { useEffect } from 'react'
+import "./Test.css";
 
 
-// useRef()
-/*
-	useRef(): "use Refrence" Does not cause re-renders when it values changes.
-		When you want a component to "remember" some information,
-		but you don't want that that information to trigger new renders.
 
-		1. Accessing/Interacting/ with DOM elements
-		2. Handling Focus, Animations and Transitions
-		3. Managing TImers, and Itervals
-	
-	
-	// Without Ref:
-		const [number, setNumber] = useState(0);
-		const handleClick = () => {
-			console.log(number);
-			setNumber(n => n + 1);
-		}
-		<button onClick={() => handleClick()}>Click Me!</button>
-	
-	NOTE: I can link ref with value and also with element
-		<input ref={InputRef} type="text"/>
-*/
-
-import "./style.css";
-
-import { useRef, useEffect } from "react";
-
-function Test() {
-	const inputRef = useRef(null);
-
+export default function Test() {
 	useEffect(() => {
-	console.log("COMPONENT RENDERED");
-	});
-
-	const handleClick = () => {
-	if (inputRef.current) {
-		inputRef.current.style.backgroundColor = "yellow";
-	}
-	};
-
-	return (
-	<div>
-		<input ref={inputRef} type="text" />
-		<button onClick={handleClick}>Click Me!</button>
-	</div>
-	);
+		console.log("Start Fecthing The Data");
+	}, []);
+  return (
+	<div>Test</div>
+  )
 }
-
-
-export default Test;
-
-
-
