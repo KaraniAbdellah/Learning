02f60ad324635aa9a16a41c0393cbf7d72704f3b
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-24 14:30:53 +0000

    add schema for user login

--- Learn Js/authen_author/src/index.js
+++ Learn Js/authen_author/src/index.js
@@ -12,69 +12,34 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 
+const templatePath = path.join(__dirname, "../templates");
+const publicPath = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.set(express.static(publicPath));
+ 
+
 // Start Making The REST API Routes
-app.get("/getUsers", async (req, res) => {
+app.get("/", async (req, res) => {
     console.log("Request Come From Axios for Getting Users");
-    // Get The All User From Database
-    try {
-        const users = await User.find();
-        res.status(201).json(users);
-        console.log(users);
-        console.log("✅ Success Get User");
-    } catch (error) {
-        res.status(500).send({message: "❌ Failed Getting User"});
-        console.log("❌ Failed Getting User" + error);
-    }
+    res.render("login");
 });
 
 
-app.delete("/deleteUser/:id", async (req, res) => {
-    console.log("Request Come From Axios for Deleting Users");
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-            console.log("Unfound User");
-            return res.status(200).send({message: "❌ Unfound User"});
-        }
-        else {
-            console.log("✅ Success Delete User");
-            return res.status(200).send({message: "✅ Success Delete User"});
-        }
-    } catch (error) {
-        res.status(500).send({message: "❌ Failed Deleting User"});
-        console.log("❌ Failed Deleting User" + error);
-    }
-});
-
-
-app.post("/postUser", async (req, res) => {
+app.get("/signup", async (req, res) => {
     console.log("Request Come From Axios for Posting Users");
-    // Start Add User To DataBase
-    const user = req.body;
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).json(user);
-        console.log("✅ Success Adding User");
-    } catch (error) {
-        res.status(500).send({message: "❌ Failed Adding User"});
-        console.log("❌ Failed Adding User" + error);
-    }
+    res.render("signup");
+});
+
+app.get("/home", async (req, res) => {
+    console.log("Request Come From Axios for Posting Users");
+    res.render("home");
 });
 
 
-app.put("/updateProduct/:id", async(req, res) => {
-    console.log("🕵️‍♀️ Request From Postman [PUT Method]");
-    const id = req.params.id;
-    const newProduct = req.body;
-    try {
-        const UpdatedData = await Product.findByIdAndUpdate(id, newProduct);
-        res.status(200).send({message: "✅ Data Updated Succefully"});
-        console.log("✅ Data Updated Succefully");
-    } catch (error) {
-        console.log("❌ Failed Updating Data" + error);
-        res.status(500).send({message: "❌ Failed Updating Data"});
-    }
+app.listen(3000, () => {
+    console.log("Server Listening to Port 3000");
 });
 
 
--- Learn Js/authen_author/src/mongo.js
+++ Learn Js/authen_author/src/mongo.js
@@ -1,2 +1,29 @@
 // Start MongoDb
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Login").then(() => {
+    console.log("Mongo Connected");
+}).catch((err) => {
+    console.log("Error + " + err);
+});
+
+
+const User = mongoose.Schema({
+    email: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+    token: {
+        type: String, 
+        required: true,
+    }
+});
+
+const Collection = new mongoose.model("UserLogin", User);
+
+module.exports = User;
+
 
