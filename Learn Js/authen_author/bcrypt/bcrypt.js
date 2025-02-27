a087fbe52814288efe4d86661012dcab045627fe
KaraniAbdellah <abdellahkarani@gmail.com>
2025-02-27 05:15:19 +0000

    add folders for start learning authentification

--- Learn Js/authen_author/bcrypt/bcrypt.js
+++ Learn Js/authen_author/bcrypt/bcrypt.js
@@ -1,25 +1,4 @@
-const bcrypt = require('bcrypt');
-
-// Authentification && Authorization
-/*
-    Authentication: proving you are who you say you are 
-    Authorization: The permission you have
-*/
-
-
-
-// Intro To Needed Tools
-/*
-    JWT (JSON Web Tokens) for handling authentication (storing tokens for user sessions).
-    bcrypt.js for hashing passwords before storing them in the database.
-    Passport.js (optional) for simplifying authentication strategies.
-
-    Start with bcrypt.js for password hashing, 
-    then use JWT for authentication, and optionally 
-    add Passport.js for simplifying strategies.
-*/
-
-
+// Bcrypt
 /*
     Hashing Algorithms Overview:
         - MD5, SHA-1 → Insecure
@@ -54,4 +33,6 @@ bcrypt.hash(plainPassword, saltRounds, function(err, hash) {
         });
     }
 });
+
+
 
