5c8cce8d94beae392ca243c890ea13ec3b2bb453
KaraniAbdellah <abdellahkarani@gmail.com>
2025-03-02 12:32:06 +0000

    start verify expiration of token

--- Learn Js/authen_author/jwt/jwt.js
+++ Learn Js/authen_author/jwt/jwt.js
@@ -6,16 +6,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const posts = [
-    {id: 1, username: "John"},
-    {id: 2, username: "Miller"},
-    {id: 3, username: "Don"},
-];
-
-app.get("/posts", authenticateToken, (req, res) => {
-    res.json(posts.filter(post => post.username === req.body.username)); // Changed req.json to res.json
-    console.log("Request Come To Get Method --> Posts");
-});
 
 app.post("/login", (req, res) => {
     console.log("Request Came To POST Method --> Login");
@@ -27,18 +17,12 @@ app.post("/login", (req, res) => {
     res.json({ accessToken: accessToken });
 });
 
-// Create Middleware --> Verify Token && Check The User
-function authenticateToken(req, res, next) { // Changed nex to next
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    if (token === null) return res.sendStatus(401); // Changed senStatus to sendStatus
 
-    jwt.verify(token, "SCREtKeY", (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
+app.post("/auth", (req, res) => {
+    // Verfiy The Expiration of The Token
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`✅ Port Running in ${process.env.PORT || 3000}`);
