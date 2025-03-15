01b8a8b184277c5c595563d2fae29b24c6acbfe1
KaraniAbdellah <abdellahkarani@gmail.com>
2025-03-15 23:42:46 +0000

    add  and  Var

--- Learn_PHP/Learn/learn.php
+++ Learn_PHP/Learn/learn.php
@@ -25,6 +25,7 @@
     echo"Online Status: {$online}";
 
 
+
     // Operators
     /*
         Arithmetic Operator [+ - * / ** %]
@@ -37,6 +38,58 @@
     $z = $x / $y; // + - * / 
     $z++; $z--;
     echo $z;
+
+
+
+    // $_GET and $_POST Varaibles
+    /*
+        $_GET, $_POST = special variables used to collect data from an HTML form
+                        data is sent to the file in the action attribute of <form>
+                        <form action='file.php' method="GET">
+
+        $_GET = Data is appended to the url
+                Not Secure
+                char limit
+                Bookmark is possible w/ values
+                GET request can be cached
+                Better for a search page
+
+        $_POST = Data is packaged inside the body of the HTTP request
+                MORE Secure
+                No Data limit
+                Cannot Bookmark
+                GET requestes are not cached
+                Better for submitting credentials 
+    */
+
+    // index.html
+    /*
+        <form action="index.php" method="post">
+            <input name="email" type="email" placeholder="enter your email">
+            <input name="password" type="password" placeholder="enter your password">
+            <input type="submit" value="submit">
+        </form>
+    */
+
+    // echo $_GET["email"] . "<br>";
+    echo "{$_GET["email"]} <br>";
+    echo "{$_GET["password"]} <br>";
+
+    // More Secure --> The Data Does Not Show in URL
+    echo "{$_POST["email"]} <br>";
+    echo "{$_POST["password"]} <br>";
+
+
+
+    // Exercice
+    
+
+
+
+
+
+
+
 ?>
 
 
