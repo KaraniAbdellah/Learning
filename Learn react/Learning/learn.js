284ef3b38cf3ace1e3e222df438e8d06e96ee92e
KaraniAbdellah <abdellahkarani@gmail.com>
2025-03-28 17:19:28 +0000

    add use Ref

--- Learn react/Learning/learn.js
+++ Learn react/Learning/learn.js
@@ -1427,6 +1427,29 @@ console.log("Pre Render");
 
 
 // useRef()
+/*
+    like useState() but don't trigger re-renders
+    refs are mutable
+    states are ummutable
+
+    // In This Example Unitil Render And Then You Can See The Result
+*/
+
+const Test = () => {
+    const ref = useRef(0);
+    console.log(ref); // {current: 0}
+    console.log(ref.current);
+    const handleIncremt = () => {
+        ref.current++;
+        console.log(ref.current);
+    }
+    return (
+        <div>
+            <button onClick={() => handleIncremt()}>click me!</button>
+            <p>{ref.current}</p>
+        </div>
+    );
+};
 
 
 
--- Learn react/practice/src/Test.jsx
+++ Learn react/practice/src/Test.jsx
@@ -5,13 +5,24 @@ import React, { useRef } from "react";
     like useState() but don't trigger re-renders
     refs are mutable
     states are ummutable
+
+    // In This Example Unitil Render And Then You Can See The Result
 */
 
 const Test = () => {
     const ref = useRef(0);
     console.log(ref); // {current: 0}
     console.log(ref.current);
-    return <div></div>;
+    const handleIncremt = () => {
+        ref.current++;
+        console.log(ref.current);
+    }
+    return (
+        <div>
+            <button onClick={() => handleIncremt()}>click me!</button>
+            <p>{ref.current}</p>
+        </div>
+    );
 };
 
 export default Test;
