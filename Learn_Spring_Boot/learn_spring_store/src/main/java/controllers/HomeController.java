47f4b1c74f506f580a90c8bf21e86add65c5e0c4
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-23 22:30:50 +0100

    add Dependency Injection

--- Learn_Spring_Boot/learn_spring_store/src/main/java/controllers/HomeController.java
+++ Learn_Spring_Boot/learn_spring_store/src/main/java/controllers/HomeController.java
@@ -1,16 +1,21 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class HomeController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println(appName); // learn_spring_store
         return "index.html";
     }
-
 
 }
 
