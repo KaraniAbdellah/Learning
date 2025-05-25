ba58e5969f38d65d0cf745933e53cd3c6da589d7
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-25 15:57:46 +0100

    start learning part2

---
+++ Learn_Spring_Boot/spring_boot_part2/src/main/java/example/com/spring_boot_part2/SpringBootPart2Application.java
@@ -0,0 +1,13 @@
+package example.com.spring_boot_part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPart2Application.class, args);
+	}
+
+}
