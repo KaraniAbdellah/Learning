9e7fca1b90b29a327cad334add844bbdc2a34b64
KaraniAbdellah <abdellahkarani@gmail.com>
2025-07-30 21:01:38 +0100

    docs: add src and intro

---
+++ Learn_TS/intro.ts
@@ -0,0 +1,15 @@
+// What is Ts:
+/*
+    is super set for js
+    at end ts will be transpiler to js
+    we need ts for runtime errors --> we can detect errors at compile time
+        compile time or static type checking [detect errors before run the code]
+    Every Js file is valid in ts file
+
+*/
+
+// Compiler vs Trandpiler
+/*
+    Compiler: from high level code to low level code
+    Transpiler: from high level code to high level code
+*/
