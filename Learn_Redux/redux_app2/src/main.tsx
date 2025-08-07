4d1f23cfd35e8a4f14bf29831ef3c99749d843c9
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-08 00:13:20 +0100

    docs: add what is redux tolkit

--- Learn_Redux/redux_app2/src/main.tsx
+++ Learn_Redux/redux_app2/src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./states/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
---
+++ Learn_Redux/redux_app2/src/states/counter/counterSlice.ts
@@ -0,0 +1,15 @@
+// this file contain every thing like actions, reducers, states, ...
+
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Counter {
+  value: number;
+}
+
+const initialState: Counter = {
+  value: 0,
+};
+
+
+const counterSlice = createSlice();
+
