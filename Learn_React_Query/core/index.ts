3939f418acb762625f4626c08683e7930fee64d7
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-11 12:32:17 +0100

    docs: start learning react query

---
+++ Learn_React_Query/core/index.ts
@@ -0,0 +1,9 @@
+// What is React Query
+/*
+    React Query is a powerful library developed by 
+    TanStack that simplifies data fetching and 
+    state management in React applications
+*/
+
+
+
