419d40b355bea1df206b9b55cdfca78976318a14
KaraniAbdellah <abdellahkarani@gmail.com>
2025-12-03 00:48:53 +0100

    feat: add What is Stack

--- Learn_React_Native/app/src/app/_layout.tsx
+++ Learn_React_Native/app/src/app/_layout.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Stack } from "expo-router";
 
+
+/*
+  --> Stack organize screens like a pile of pages
+  --> Expo use filies as Screens
+  --> _layout.js: if file when we defien how screen arranged
+  --> Use <Link /> or router.push()
+*/
+
+
 const RootLayout = () => {
   return <Stack>
     <Stack.Screen name="index" options={{title: "Home"}}></Stack.Screen>
     <Stack.Screen name="details" options={{title: "Details"}}></Stack.Screen>
   </Stack>;
 };
+
 
 export default RootLayout;
