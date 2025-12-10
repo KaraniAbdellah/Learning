03b99e974ac44360d40ff49612001ae9a7e96b77
KaraniAbdellah <abdellahkarani@gmail.com>
2025-12-10 18:08:28 +0100

    feat: add Screens and navigation

---
+++ Learn_React_Native/navigation-app/app/Navigation.ts
@@ -0,0 +1,6 @@
+// App Navigation
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function Navigation() {
+  return <NavigationContainer></NavigationContainer>; 
+}
